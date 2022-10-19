9cf3c24c992145288b5f0d14348b6cb5e4686735
猿人易 <qcjxberin@outlook.com>
2022-10-19 11:01:37 +0800

    DG.PluginCore.IPlugins.AspNetCore

---
+++ src/DG.PluginCore.IPlugins.AspNetCore/IPlugins/IContentFilterPlugin.cs
@@ -0,0 +1,12 @@
+﻿namespace PluginCore.IPlugins
+{
+    /// <summary>
+    /// 目前未有效化，占坑
+    /// </summary>
+    public interface IContentFilterPlugin : IPlugin
+    {
+        Task<string> RequestBodyFilter(string urlPath, string content);
+
+        Task<string> ReponseBodyFilter(string urlPath, string content);
+    }
+}
---
+++ src/DG.PluginCore.IPlugins.AspNetCore/IPlugins/IHttpFilterPlugin.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace PluginCore.IPlugins
+{
+    /// <summary>
+    /// 实验性: 不确定有效, 发现问题，请 new issue
+    /// </summary>
+    public interface IHttpFilterPlugin : IPlugin
+    {
+        Task HttpEndFilter(HttpContext httpContext);
+
+        Task HttpStartFilter(HttpContext httpContext);
+    }
+}
