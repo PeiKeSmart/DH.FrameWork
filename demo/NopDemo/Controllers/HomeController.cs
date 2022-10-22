8ccf192a9b0ebf28f43138edad1188be1ca839ab
猿人易 <qcjxberin@outlook.com>
2022-10-22 11:56:15 +0800

    增加清除插件的库

---
+++ demo/NopDemo/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using System.Diagnostics;
+
+namespace NopDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
