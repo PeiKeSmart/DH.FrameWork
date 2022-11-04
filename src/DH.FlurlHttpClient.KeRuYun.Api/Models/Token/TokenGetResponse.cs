9ccd5fb49fab0121674be86b307b07e16d80f240
猿人易 <qcjxberin@outlook.com>
2022-11-04 19:55:45 +0800

    增加客如云Demo

--- src/DH.FlurlHttpClient.KeRuYun.Api/Models/Token/TokenGetResponse.cs
+++ src/DH.FlurlHttpClient.KeRuYun.Api/Models/Token/TokenGetResponse.cs
@@ -5,5 +5,44 @@
 /// </summary>
 public class TokenGetResponse: KeRuYunApiResponse
 {
+    public static class Types
+    {
+        public class Result
+        {
+            /// <summary>
+            /// 获取或设置 token。
+            /// </summary>
+            [Newtonsoft.Json.JsonProperty("token")]
+            [System.Text.Json.Serialization.JsonPropertyName("token")]
+            public String? Token { get; set; }
+        }
+    }
 
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    [Newtonsoft.Json.JsonProperty("message")]
+    [System.Text.Json.Serialization.JsonPropertyName("message")]
+    public String? Message { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    [Newtonsoft.Json.JsonProperty("apiMessage")]
+    [System.Text.Json.Serialization.JsonPropertyName("apiMessage")]
+    public String? ApiMessage { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    [Newtonsoft.Json.JsonProperty("messageUuid")]
+    [System.Text.Json.Serialization.JsonPropertyName("messageUuid")]
+    public String? MessageUuid { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    [Newtonsoft.Json.JsonProperty("result")]
+    [System.Text.Json.Serialization.JsonPropertyName("result")]
+    public Types.Result? Result { get; set; }
 }
