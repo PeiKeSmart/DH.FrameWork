10f9bb340fd0539f3b3c95ed6d091aca80e19066
猿人易 <qcjxberin@outlook.com>
2022-11-05 22:12:57 +0800

    优化ToDateTime，遇到很大数字时，转时间无效 优化FieldSize，支持多FieldSize，支持FieldSize=-1表示在某个版本中忽略该字段 完整的Json序列化栈不成熟 IDictionarySource 支持Json序列化扩展 序列化字典数据源时，也要支持驼峰命名

--- src/DH.NCore/Common/Utility.cs
+++ src/DH.NCore/Common/Utility.cs
@@ -165,6 +165,8 @@ namespace System
     {
         private static readonly DateTime _dt1970 = new(1970, 1, 1);
         private static readonly DateTimeOffset _dto1970 = new(new DateTime(1970, 1, 1));
+        private static readonly Int64 _maxSeconds = (Int64)(DateTime.MaxValue - DateTime.MinValue).TotalSeconds;
+        private static readonly Int64 _maxMilliseconds = (Int64)(DateTime.MaxValue - DateTime.MinValue).TotalMilliseconds;
 
         /// <summary>转为整数，转换失败时返回默认值。支持字符串、全角、字节数组（小端）、时间（Unix秒）</summary>
         /// <param name="value">待转换对象</param>
@@ -194,13 +196,21 @@ namespace System
 
                 //// 先转UTC时间再相减，以得到绝对时间差
                 //return (Int32)(dt.ToUniversalTime() - _dt1970).TotalSeconds;
-                return (Int32)(dt - _dt1970).TotalSeconds;
+                // 保存时间日期由Int32改为UInt32，原截止2038年的范围扩大到2106年
+                var n = (dt - _dt1970).TotalSeconds;
+                if (n >= Int32.MaxValue) throw new InvalidDataException("时间过大，数值超过Int32.MaxValue");
+
+                return (Int32)n;
             }
             if (value is DateTimeOffset dto)
             {
                 if (dto == DateTimeOffset.MinValue) return 0;
 
-                return (Int32)(dto - _dto1970).TotalSeconds;
+                //return (Int32)(dto - _dto1970).TotalSeconds;
+                var n = (dto - _dto1970).TotalSeconds;
+                if (n >= Int32.MaxValue) throw new InvalidDataException("时间过大，数值超过Int32.MaxValue");
+
+                return (Int32)n;
             }
 
             if (value is Byte[] buf)
@@ -447,9 +457,16 @@ namespace System
                 return dt;
             }
             // 特殊处理整数，Unix秒，绝对时间差，不考虑UTC时间和本地时间。
-            if (value is Int32 k) return k == 0 ? DateTime.MinValue : _dt1970.AddSeconds(k);
+            if (value is Int32 k)
+            {
+                if (k >= _maxSeconds || k <= -_maxSeconds) return defaultValue;
+
+                return _dt1970.AddSeconds(k);
+            }
             if (value is Int64 m)
             {
+                if (m >= _maxMilliseconds || m <= -_maxMilliseconds) return defaultValue;
+
                 if (m > 100 * 365 * 24 * 3600L)
                     return _dt1970.AddMilliseconds(m);
                 else
@@ -460,7 +477,10 @@ namespace System
             {
                 return Convert.ToDateTime(value);
             }
-            catch { return defaultValue; }
+            catch
+            {
+                return defaultValue;
+            }
         }
 
         /// <summary>转为时间日期，转换失败时返回最小时间。支持字符串、整数（Unix秒）</summary>
@@ -487,9 +507,16 @@ namespace System
                 return defaultValue;
             }
             // 特殊处理整数，Unix秒，绝对时间差，不考虑UTC时间和本地时间。
-            if (value is Int32 k) return k == 0 ? DateTimeOffset.MinValue : _dto1970.AddSeconds(k);
+            if (value is Int32 k)
+            {
+                if (k >= _maxSeconds || k <= -_maxSeconds) return defaultValue;
+
+                return _dto1970.AddSeconds(k);
+            }
             if (value is Int64 m)
             {
+                if (m >= _maxMilliseconds || m <= -_maxMilliseconds) return defaultValue;
+
                 if (m > 100 * 365 * 24 * 3600L)
                     return _dto1970.AddMilliseconds(m);
                 else
@@ -500,7 +527,10 @@ namespace System
             {
                 return Convert.ToDateTime(value);
             }
-            catch { return defaultValue; }
+            catch
+            {
+                return defaultValue;
+            }
         }
 
         /// <summary>全角为半角</summary>
