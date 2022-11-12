f1047dfb41303b8a660476299539bb81b857e49d
猿人易 <qcjxberin@outlook.com>
2022-11-12 20:07:30 +0800

    增加SignalR类

---
+++ src/DH.SignalR/wwwroot/js/initSignalR.js
@@ -0,0 +1,53 @@
+﻿/**
+ * 初始化连接
+ * @param {object} option 参数
+ * @param {string} option.url 连接的url地址
+ * @param {string} option.loggingLevel 日志级别,默认为 Error
+ * @param {number} option.delay 延迟连接 默认为3000毫秒
+ * @param {function} option.onStarted 启动时触发
+ * @param {function} option.onLine 启动时触发
+ * @param {function} option.offLine 启动时触发
+ * @returns {object} 连接的实例
+ */
+function initSignalr(option) {
+    var config = Object.assign(true, {
+        loggingLevel: signalR.LogLevel.Error,
+        delay: 3000,
+        url: ''
+    }, option);
+
+    var connection = new signalR.HubConnectionBuilder()
+        .configureLogging(config.loggingLevel)
+        .withUrl(config.url, {
+            accessTokenFactory: () => config.accessTokenFactory
+        })
+        .withHubProtocol(new signalR.protocols.msgpack.MessagePackHubProtocol())
+        .withAutomaticReconnect([0, 2000, 5000, 10000, 20000, 60000])
+        .build();
+
+    connection.onreconnecting(function (info) {
+        console.info('----------------------------------signalr-- onreconnecting', info);
+    });
+
+    connection.onclose(function (err) {
+        location.reload();
+        console.info('--------------------------------signalr-- onclose', err);
+    });
+
+    connection.on('OnNotify', config.onNotify);
+
+    connection.on('OnLine', config.onLine);
+
+    connection.on('OffLine', config.offLine);
+
+    setTimeout(function () {
+        connection.start().then(function (data) {
+            option.onStarted && option.onStarted(data);
+            console.log("连接成功");
+        }).catch(function (error) {
+            console.error(error.toString());
+        });
+    }, option.delay);
+
+    return connection;
+}
