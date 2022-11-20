7d53337b4b0fe1b7099caf632f9fd0714d7e6210
猿人易 <qcjxberin@outlook.com>
2022-11-20 09:27:55 +0800

    捕获线程池异常，避免应用崩溃

--- src/DH.NRemoting/Controllers/ApiController.cs
+++ src/DH.NRemoting/Controllers/ApiController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
+﻿using System.Diagnostics;
 using System.Net.NetworkInformation;
 using System.Reflection;
 using NewLife.Collections;
