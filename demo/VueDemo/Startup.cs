515c3586d0f914864241fb1f448d346b5e52e467
川流云 <qcjxberin@outlook.com>
2022-11-27 13:11:56 +0800

    暂存

--- demo/VueDemo/Startup.cs
+++ demo/VueDemo/Startup.cs
@@ -15,78 +15,77 @@ using Microsoft.Extensions.Logging;
 
 using VueCliMiddleware;
 
-namespace VueCliSample
+namespace VueCliSample;
+
+public class Startup
 {
-    public class Startup
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        // NOTE: PRODUCTION Ensure this is the same path that is specified in your webpack output
+        services.AddSpaStaticFiles(opt => opt.RootPath = "ClientApp/dist");
+        services.AddControllers();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
         {
-            Configuration = configuration;
+            app.UseDeveloperExceptionPage();
         }
 
-        public IConfiguration Configuration { get; }
+        // NOTE: this is optional, it adds HTTPS to Kestrel
+        // app.UseHttpsRedirection();
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            // NOTE: PRODUCTION Ensure this is the same path that is specified in your webpack output
-            services.AddSpaStaticFiles(opt => opt.RootPath = "ClientApp/dist");
-            services.AddControllers();
-        }
+        // NOTE: PRODUCTION uses webpack static files
+        app.UseSpaStaticFiles();
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
         {
+            endpoints.MapControllers();
+
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                app.UseSpa(spa =>
+                {
+                    spa.Options.SourcePath = "ClientApp";
+                    spa.UseVueCli(npmScript: "serve", port: 8080); // optional port
+                });
             }
-
-            // NOTE: this is optional, it adds HTTPS to Kestrel
-            // app.UseHttpsRedirection();
-
-            // NOTE: PRODUCTION uses webpack static files
-            app.UseSpaStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
+            else
             {
-                endpoints.MapControllers();
-
-                if (env.IsDevelopment())
-                {
-                    app.UseSpa(spa =>
-                    {
-                        spa.Options.SourcePath = "ClientApp";
-                        spa.UseVueCli(npmScript: "serve", port: 8080); // optional port
-                    });
-                }
-                else
-                {
-                    // NOTE: VueCliProxy is meant for developement and hot module reload
-                    // NOTE: SSR has not been tested
-                    // Production systems should only need the UseSpaStaticFiles() (above)
-                    // You could wrap this proxy in either
-                    // if (System.Diagnostics.Debugger.IsAttached)
-                    // or a preprocessor such as #if DEBUG
-                    endpoints.MapToVueCliProxy(
-                        "{*path}",
-                        new SpaOptions { SourcePath = "ClientApp" },
-                        npmScript: (System.Diagnostics.Debugger.IsAttached) ? "serve" : null,
-                        regex: "Compiled successfully",
-                        forceKill: true
-                        );
-                }
+                // NOTE: VueCliProxy is meant for developement and hot module reload
+                // NOTE: SSR has not been tested
+                // Production systems should only need the UseSpaStaticFiles() (above)
+                // You could wrap this proxy in either
+                // if (System.Diagnostics.Debugger.IsAttached)
+                // or a preprocessor such as #if DEBUG
+                endpoints.MapToVueCliProxy(
+                    "{*path}",
+                    new SpaOptions { SourcePath = "ClientApp" },
+                    npmScript: (System.Diagnostics.Debugger.IsAttached) ? "serve" : null,
+                    regex: "Compiled successfully",
+                    forceKill: true
+                    );
+            }
 #if DEBUG
 
 #else
-                
+            
 #endif
 
-            });
-        }
+        });
     }
 }
