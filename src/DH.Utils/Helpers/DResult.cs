65a4e5c205fb9ad1197bcae9e206544bacc325b6
川流云 <qcjxberin@outlook.com>
2022-12-10 15:22:14 +0800

    增加通用接口返回消息对接格式

---
+++ src/DH.Utils/Helpers/DResult.cs
@@ -0,0 +1,36 @@
+﻿namespace DH.Helpers;
+
+public class DResult
+{
+    public bool success { get; set; }
+
+    public string msg { get; set; }
+
+    /// <summary>
+    /// 状态
+    /// <para>1表成功</para>
+    /// </summary>
+    public int status { get; set; }
+
+    /// <summary>
+    /// 数据
+    /// </summary>
+    public object data { get; set; }
+
+    /// <summary>
+    /// 附加数据
+    /// </summary>
+    public object extdata { get; set; }
+
+    public int code { get; set; }
+
+    /// <summary>
+    /// 网址路径
+    /// </summary>
+    public String locate { get; set; }
+}
+
+public class DResult<T> : DResult
+{
+    public T TData { get; set; }
+}
