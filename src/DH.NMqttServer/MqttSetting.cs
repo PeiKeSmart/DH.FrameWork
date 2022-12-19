f8813f764d123a94d5806c521f3d4bea49e0c995
川流云 <qcjxberin@outlook.com>
2022-12-19 17:49:29 +0800

    增加-reinstall，支持重新安装。StarAgent升级的时候，可能需要从StarAgent.exe重新安装到dotnet StarAgent.dll

---
+++ src/DH.NMqttServer/MqttSetting.cs
@@ -0,0 +1,18 @@
+﻿using NewLife.Configuration;
+using NewLife.Xml;
+
+using System.ComponentModel;
+
+namespace DH.NMqttServer;
+
+[Config("MqttServer")]
+public class MqttSetting : XmlConfig<MqttSetting>
+{
+    /// <summary>调试开关。默认 false</summary>
+    [Description("调试开关。默认 false")]
+    public Boolean Debug { get; set; }
+
+    /// <summary>端口。默认 1883</summary>
+    [Description("端口。默认 1883")]
+    public Int32 Port { get; set; } = 1883;
+}
