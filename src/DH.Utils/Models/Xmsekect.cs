7730a793f7d24fd41d47f8a924c9248f8fa9df23
川流云 <qcjxberin@outlook.com>
2022-12-20 15:01:55 +0800

    增加下拉选项公共类

---
+++ src/DH.Utils/Models/Xmsekect.cs
@@ -0,0 +1,13 @@
+﻿namespace DH.Models;
+
+/// <summary>
+/// 下拉选项
+/// </summary>
+public class Xmsekect<TModel>
+{
+    public string name { get; set; }
+
+    public TModel value { get; set; }
+
+    public bool disabled { get; set; } = false;
+}
