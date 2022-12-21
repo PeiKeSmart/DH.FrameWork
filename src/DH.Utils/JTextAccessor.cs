26b44e6ffb9d279d88e84f3082586e04db7b8b9b
川流云 <qcjxberin@outlook.com>
2022-12-21 09:12:35 +0800

    增加通过Dynamic访问System.Text.Json对象方法

---
+++ src/DH.Utils/JTextAccessor.cs
@@ -0,0 +1,55 @@
+﻿using System.Dynamic;
+using System.Text.Json;
+
+namespace DH;
+
+/// <summary>
+/// 通过Dynamic访问System.Text.Json对象
+/// 参考：https://blog.51cto.com/shanyou/3048736
+/// </summary>
+public class JTextAccessor : DynamicObject
+{
+    private readonly JsonElement _content;
+
+    public JTextAccessor(JsonElement content)
+    {
+        _content = content;
+    }
+
+    public override bool TryGetMember(GetMemberBinder binder, out object result)
+    {
+        result = null;
+        if (_content.TryGetProperty(binder.Name, out JsonElement value))
+        {
+            result = Obtain(value);
+        }
+        else return false;
+        return true;
+    }
+
+    private object Obtain(in JsonElement element)
+    {
+        switch (element.ValueKind)
+        {
+            case JsonValueKind.String: return element.GetString();
+            case JsonValueKind.Null: return null;
+            case JsonValueKind.False: return false;
+            case JsonValueKind.True: return true;
+            case JsonValueKind.Number: return element.GetDouble();
+            default: break;
+        }
+
+        if (element.ValueKind == JsonValueKind.Array)
+        {
+            var list = new List<object>();
+            foreach (var item in element.EnumerateArray())
+            {
+                list.Add(Obtain(item));
+            }
+
+            return list;
+        }
+        // Undefine、Object
+        else return new JTextAccessor(element);
+    }
+}
