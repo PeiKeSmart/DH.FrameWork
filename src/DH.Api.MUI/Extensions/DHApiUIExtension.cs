f871a8ac75d47a3989b511a5df74e2294fc0e555
川流云 <qcjxberin@outlook.com>
2022-12-25 11:21:17 +0800

    增加Api Ui

---
+++ src/DH.Api.MUI/Extensions/DHApiUIExtension.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace DH.Api.MUI.Extensions;
+
+public static class DHApiUIExtension
+{
+    public static IApplicationBuilder UseFytApiUI(this IApplicationBuilder app, Action<ApiUIOptions> setupAction = null)
+    {
+        var options = new ApiUIOptions();
+        if (setupAction != null)
+        {
+            setupAction(options);
+        }
+        else
+        {
+            options = app.ApplicationServices.GetRequiredService<IOptions<ApiUIOptions>>().Value;
+        }
+        app.UseMiddleware<ApiUIMiddleware>(options);
+        return app;
+    }
+}
