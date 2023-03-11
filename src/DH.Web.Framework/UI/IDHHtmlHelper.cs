84a1236247e1ca6caa5174a15d23afe7379e3844
猿人易 <qcjxberin@outlook.com>
2023-03-11 16:51:19 +0800

    完成HTML帮助程序

--- src/DH.Web.Framework/UI/IDHHtmlHelper.cs
+++ src/DH.Web.Framework/UI/IDHHtmlHelper.cs
@@ -2,224 +2,225 @@
 
 using System.Reflection;
 
-namespace DH.Web.Framework.UI
-{
+namespace DH.Web.Framework.UI;
+
+/// <summary>
+/// 表示HTML帮助程序
+/// </summary>
+public partial interface IDHHtmlHelper {
     /// <summary>
-    /// 表示HTML帮助程序
+    /// 将title元素添加到<![CDATA[<head>]]>
     /// </summary>
-    public partial interface IDHHtmlHelper
-    {
-        /// <summary>
-        /// 将标题元素添加到<![CDATA[<head>]]>
-        /// </summary>
-        /// <param name="part">Title part</param>
-        void AddTitleParts(string part);
+    /// <param name="part">Title part</param>
+    void AddTitleParts(string part);
 
-        /// <summary>
-        /// 将标题元素附加到<![CDATA[<head>]]>
-        /// </summary>
-        /// <param name="part">Title part</param>
-        void AppendTitleParts(string part);
+    /// <summary>
+    /// 将title元素附加到<![CDATA[<head>]]>
+    /// </summary>
+    /// <param name="part">Title part</param>
+    void AppendTitleParts(string part);
 
-        /// <summary>
-        /// 生成所有标题部分
-        /// </summary>
-        /// <param name="addDefaultTitle">指示是否插入默认标题的值</param>
-        /// <param name="part">标题部分</param>
-        /// <returns>生成的HTML字符串</returns>
-        IHtmlContent GenerateTitle(bool addDefaultTitle = true, string part = "");
+    /// <summary>
+    /// 生成所有标题部分
+    /// </summary>
+    /// <param name="addDefaultTitle">指示是否插入默认标题的值</param>
+    /// <param name="part">标题部分</param>
+    /// <returns>表示异步操作的任务
+    /// 任务结果包含生成的HTML字符串</returns>
+    Task<IHtmlContent> GenerateTitleAsync(bool addDefaultTitle = true, string part = "");
 
-        /// <summary>
-        /// 将元描述元素添加到<![CDATA[<head>]]>
-        /// </summary>
-        /// <param name="part">元描述部分</param>
-        void AddMetaDescriptionParts(string part);
+    /// <summary>
+    /// 将元描述元素添加到<![CDATA[<head>]]>
+    /// </summary>
+    /// <param name="part">元描述部分</param>
+    void AddMetaDescriptionParts(string part);
 
-        /// <summary>
-        /// 将元描述元素附加到<![CDATA[<head>]]>
-        /// </summary>
-        /// <param name="part">元描述部分</param>
-        void AppendMetaDescriptionParts(string part);
+    /// <summary>
+    /// 将元描述元素附加到<![CDATA[<head>]]>
+    /// </summary>
+    /// <param name="part">元描述部分</param>
+    void AppendMetaDescriptionParts(string part);
 
-        /// <summary>
-        /// 生成所有描述部分
-        /// </summary>
-        /// <param name="part">元描述部分</param>
-        /// <returns>生成的HTML字符串</returns>
-        IHtmlContent GenerateMetaDescription(string part = "");
+    /// <summary>
+    /// 生成所有描述部分
+    /// </summary>
+    /// <param name="part">元描述部分</param>
+    /// <returns>表示异步操作的任务
+    /// 任务结果包含生成的HTML字符串</returns>
+    Task<IHtmlContent> GenerateMetaDescriptionAsync(string part = "");
 
-        /// <summary>
-        /// Add meta keyword element to the <![CDATA[<head>]]>
-        /// </summary>
-        /// <param name="part">Meta keyword part</param>
-        void AddMetaKeywordParts(string part);
+    /// <summary>
+    /// 将meta关键字元素添加到<![CDATA[<head>]]>
+    /// </summary>
+    /// <param name="part">元关键字部分</param>
+    void AddMetaKeywordParts(string part);
 
-        /// <summary>
-        /// Append meta keyword element to the <![CDATA[<head>]]>
-        /// </summary>
-        /// <param name="part">Meta keyword part</param>
-        void AppendMetaKeywordParts(string part);
+    /// <summary>
+    /// 将meta关键字元素附加到<![CDATA[<head>]]>
+    /// </summary>
+    /// <param name="part">元关键字部分</param>
+    void AppendMetaKeywordParts(string part);
 
-        /// <summary>
-        /// Generate all keyword parts
-        /// </summary>
-        /// <param name="part">Meta keyword part</param>
-        /// <returns>Generated HTML string</returns>
-        IHtmlContent GenerateMetaKeywords(string part = "");
+    /// <summary>
+    /// 生成所有关键字部分
+    /// </summary>
+    /// <param name="part">元关键字部分</param>
+    /// <returns>表示异步操作的任务
+    /// 任务结果包含生成的HTML字符串</returns>
+    Task<IHtmlContent> GenerateMetaKeywordsAsync(string part = "");
 
-        /// <summary>
-        /// Add script element
-        /// </summary>
-        /// <param name="location">A location of the script element</param>
-        /// <param name="src">Script path (minified version)</param>
-        /// <param name="debugSrc">Script path (full debug version). If empty, then minified version will be used</param>
-        /// <param name="excludeFromBundle">A value indicating whether to exclude this script from bundling</param>
-        void AddScriptParts(ResourceLocation location, string src, string debugSrc = "", bool excludeFromBundle = false);
+    /// <summary>
+    /// 添加脚本元素
+    /// </summary>
+    /// <param name="location">脚本元素的位置</param>
+    /// <param name="src">脚本路径（缩小版本）</param>
+    /// <param name="debugSrc">脚本路径（完整调试版本）。如果为空，则将使用缩小版本</param>
+    /// <param name="excludeFromBundle">指示是否从绑定中排除此脚本的值</param>
+    void AddScriptParts(ResourceLocation location, string src, string debugSrc = "", bool excludeFromBundle = false);
 
-        /// <summary>
-        /// Append script element
-        /// </summary>
-        /// <param name="location">A location of the script element</param>
-        /// <param name="src">Script path (minified version)</param>
-        /// <param name="debugSrc">Script path (full debug version). If empty, then minified version will be used</param>
-        /// <param name="excludeFromBundle">A value indicating whether to exclude this script from bundling</param>
-        void AppendScriptParts(ResourceLocation location, string src, string debugSrc = "", bool excludeFromBundle = false);
+    /// <summary>
+    /// 追加脚本元素
+    /// </summary>
+    /// <param name="location">脚本元素的位置</param>
+    /// <param name="src">脚本路径（缩小版本）</param>
+    /// <param name="debugSrc">脚本路径（完整调试版本）。如果为空，则将使用缩小版本</param>
+    /// <param name="excludeFromBundle">指示是否从绑定中排除此脚本的值</param>
+    void AppendScriptParts(ResourceLocation location, string src, string debugSrc = "", bool excludeFromBundle = false);
 
-        /// <summary>
-        /// Generate all script parts
-        /// </summary>
-        /// <param name="location">A location of the script element</param>
-        /// <returns>Generated HTML string</returns>
-        IHtmlContent GenerateScripts(ResourceLocation location);
+    /// <summary>
+    /// 生成所有脚本部分
+    /// </summary>
+    /// <param name="location">脚本元素的位置</param>
+    /// <returns>生成的HTML字符串</returns>
+    IHtmlContent GenerateScripts(ResourceLocation location);
 
-        /// <summary>
-        /// Add inline script element
-        /// </summary>
-        /// <param name="location">A location of the script element</param>
-        /// <param name="script">Script</param>
-        void AddInlineScriptParts(ResourceLocation location, string script);
+    /// <summary>
+    /// 添加内联脚本元素
+    /// </summary>
+    /// <param name="location">脚本元素的位置</param>
+    /// <param name="script">Script</param>
+    void AddInlineScriptParts(ResourceLocation location, string script);
 
-        /// <summary>
-        /// Append inline script element
-        /// </summary>
-        /// <param name="location">A location of the script element</param>
-        /// <param name="script">Script</param>
-        void AppendInlineScriptParts(ResourceLocation location, string script);
+    /// <summary>
+    /// 附加内联脚本元素
+    /// </summary>
+    /// <param name="location">脚本元素的位置</param>
+    /// <param name="script">Script</param>
+    void AppendInlineScriptParts(ResourceLocation location, string script);
 
-        /// <summary>
-        /// Generate all inline script parts
-        /// </summary>
-        /// <param name="location">A location of the script element</param>
-        /// <returns>Generated HTML string</returns>
-        IHtmlContent GenerateInlineScripts(ResourceLocation location);
+    /// <summary>
+    /// 生成所有内联脚本部分
+    /// </summary>
+    /// <param name="location">脚本元素的位置</param>
+    /// <returns>生成的HTML字符串</returns>
+    IHtmlContent GenerateInlineScripts(ResourceLocation location);
 
-        /// <summary>
-        /// Add CSS element
-        /// </summary>
-        /// <param name="src">Script path (minified version)</param>
-        /// <param name="debugSrc">Script path (full debug version). If empty, then minified version will be used</param>
-        /// <param name="excludeFromBundle">A value indicating whether to exclude this style sheet from bundling</param>
-        void AddCssFileParts(string src, string debugSrc = "", bool excludeFromBundle = false);
+    /// <summary>
+    /// 添加CSS元素
+    /// </summary>
+    /// <param name="src">脚本路径（缩小版本）</param>
+    /// <param name="debugSrc">脚本路径（完整调试版本）。如果为空，则将使用缩小版本</param>
+    /// <param name="excludeFromBundle">指示是否从绑定中排除此样式表的值</param>
+    void AddCssFileParts(string src, string debugSrc = "", bool excludeFromBundle = false);
 
-        /// <summary>
-        /// Append CSS element
-        /// </summary>
-        /// <param name="src">Script path (minified version)</param>
-        /// <param name="debugSrc">Script path (full debug version). If empty, then minified version will be used</param>
-        /// <param name="excludeFromBundle">A value indicating whether to exclude this style sheet from bundling</param>
-        void AppendCssFileParts(string src, string debugSrc = "", bool excludeFromBundle = false);
+    /// <summary>
+    /// 附加CSS元素
+    /// </summary>
+    /// <param name="src">脚本路径（缩小版本）</param>
+    /// <param name="debugSrc">脚本路径（完整调试版本）。如果为空，则将使用缩小版本</param>
+    /// <param name="excludeFromBundle">指示是否从绑定中排除此样式表的值</param>
+    void AppendCssFileParts(string src, string debugSrc = "", bool excludeFromBundle = false);
 
-        /// <summary>
-        /// Generate all CSS parts
-        /// </summary>
-        /// <returns>Generated HTML string</returns>
-        IHtmlContent GenerateCssFiles();
+    /// <summary>
+    /// 生成所有CSS部分
+    /// </summary>
+    /// <returns>生成的HTML字符串</returns>
+    IHtmlContent GenerateCssFiles();
 
-        /// <summary>
-        /// Add canonical URL element to the <![CDATA[<head>]]>
-        /// </summary>
-        /// <param name="part">Canonical URL part</param>
-        /// <param name="withQueryString">Whether to use canonical URLs with query string parameters</param>
-        void AddCanonicalUrlParts(string part, bool withQueryString = false);
+    /// <summary>
+    /// 将规范URL元素添加到<![CDATA[<head>]]>
+    /// </summary>
+    /// <param name="part">标准URL部分</param>
+    /// <param name="withQueryString">是否将规范URL与查询字符串参数一起使用</param>
+    void AddCanonicalUrlParts(string part, bool withQueryString = false);
 
-        /// <summary>
-        /// Append canonical URL element to the <![CDATA[<head>]]>
-        /// </summary>
-        /// <param name="part">Canonical URL part</param>
-        void AppendCanonicalUrlParts(string part);
+    /// <summary>
+    /// 将规范URL元素附加到<![CDATA[<head>]]>
+    /// </summary>
+    /// <param name="part">标准URL部分</param>
+    void AppendCanonicalUrlParts(string part);
 
-        /// <summary>
-        /// Generate all canonical URL parts
-        /// </summary>
-        /// <returns>Generated HTML string</returns>
-        IHtmlContent GenerateCanonicalUrls();
+    /// <summary>
+    /// 生成所有规范URL部分
+    /// </summary>
+    /// <returns>生成的HTML字符串</returns>
+    IHtmlContent GenerateCanonicalUrls();
 
-        /// <summary>
-        /// Add any custom element to the <![CDATA[<head>]]> element
-        /// </summary>
-        /// <param name="part">The entire element. For example, <![CDATA[<meta name="msvalidate.01" content="123121231231313123123" />]]></param>
-        void AddHeadCustomParts(string part);
+    /// <summary>
+    /// 向元素<![CDATA[<head>]]>中添加任何自定义元素
+    /// </summary>
+    /// <param name="part">整个元素。例如<![CDATA[<meta name="msvalidate.01" content="123121231231313123123" />]]></param>
+    void AddHeadCustomParts(string part);
 
-        /// <summary>
-        /// Append any custom element to the <![CDATA[<head>]]> element
-        /// </summary>
-        /// <param name="part">The entire element. For example, <![CDATA[<meta name="msvalidate.01" content="123121231231313123123" />]]></param>
-        void AppendHeadCustomParts(string part);
+    /// <summary>
+    /// 将任何自定义元素附加到<![CDATA[<head>]]>元素
+    /// </summary>
+    /// <param name="part">整个元素。例如<![CDATA[<meta name="msvalidate.01" content="123121231231313123123" />]]></param>
+    void AppendHeadCustomParts(string part);
 
-        /// <summary>
-        /// Generate all custom elements
-        /// </summary>
-        /// <returns>Generated HTML string</returns>
-        IHtmlContent GenerateHeadCustom();
+    /// <summary>
+    /// 生成所有自定义元素
+    /// </summary>
+    /// <returns>生成的HTML字符串</returns>
+    IHtmlContent GenerateHeadCustom();
 
-        /// <summary>
-        /// Add CSS class to the <![CDATA[<head>]]> element
-        /// </summary>
-        /// <param name="part">CSS class</param>
-        void AddPageCssClassParts(string part);
+    /// <summary>
+    /// 将CSS类添加到<![CDATA[<head>]]>元素
+    /// </summary>
+    /// <param name="part">CSS class</param>
+    void AddPageCssClassParts(string part);
 
-        /// <summary>
-        /// Append CSS class to the <![CDATA[<head>]]> element
-        /// </summary>
-        /// <param name="part">CSS class</param>
-        void AppendPageCssClassParts(string part);
+    /// <summary>
+    /// 将CSS类附加到<![CDATA[<head>]]>元素
+    /// </summary>
+    /// <param name="part">CSS class</param>
+    void AppendPageCssClassParts(string part);
 
-        /// <summary>
-        /// Generate all title parts
-        /// </summary>
-        /// <param name="part">CSS class</param>
-        /// <returns>Generated string</returns>
-        string GeneratePageCssClasses(string part = "");
+    /// <summary>
+    /// 生成所有标题部分
+    /// </summary>
+    /// <param name="part">CSS类</param>
+    /// <returns>生成的字符串</returns>
+    string GeneratePageCssClasses(string part = "");
 
-        /// <summary>
-        /// Specify "edit page" URL
-        /// </summary>
-        /// <param name="url">URL</param>
-        void AddEditPageUrl(string url);
+    /// <summary>
+    /// 指定“编辑页面”URL
+    /// </summary>
+    /// <param name="url">URL</param>
+    void AddEditPageUrl(string url);
 
-        /// <summary>
-        /// Get "edit page" URL
-        /// </summary>
-        /// <returns>URL</returns>
-        string GetEditPageUrl();
+    /// <summary>
+    /// 获取“编辑页”URL
+    /// </summary>
+    /// <returns>URL</returns>
+    string GetEditPageUrl();
 
-        /// <summary>
-        /// Specify system name of admin menu item that should be selected (expanded)
-        /// </summary>
-        /// <param name="systemName">System name</param>
-        void SetActiveMenuItemSystemName(string systemName);
+    /// <summary>
+    /// 指定应选择（展开）的管理菜单项的系统名称
+    /// </summary>
+    /// <param name="systemName">系统名称</param>
+    void SetActiveMenuItemSystemName(string systemName);
 
-        /// <summary>
-        /// Get system name of admin menu item that should be selected (expanded)
-        /// </summary>
-        /// <returns>System name</returns>
-        string GetActiveMenuItemSystemName();
+    /// <summary>
+    /// 获取应选择（展开）的管理菜单项的系统名称
+    /// </summary>
+    /// <returns>系统名称</returns>
+    string GetActiveMenuItemSystemName();
 
-        /// <summary>
-        /// Get the route name associated with the request rendering this page
-        /// </summary>
-        /// <param name="handleDefaultRoutes">A value indicating whether to build the name using engine information unless otherwise specified</param>
-        /// <returns>Route name</returns>
-        string GetRouteName(bool handleDefaultRoutes = false);
-    }
+    /// <summary>
+    /// 获取与呈现此页面的请求关联的路由名称
+    /// </summary>
+    /// <param name="handleDefaultRoutes">一个值，指示是否使用引擎信息生成名称，除非另有指定</param>
+    /// <returns>路由名称</returns>
+    string GetRouteName(bool handleDefaultRoutes = false);
 }
--- src/DH.Web.Framework/WebOptimizer/Processors/DHJsMinifier.cs
+++ src/DH.Web.Framework/WebOptimizer/Processors/DHJsMinifier.cs
@@ -1,65 +1,62 @@
-﻿using DH.Core.Infrastructure;
-using NUglify.JavaScript;
-using NUglify;
-using WebOptimizer;
-using DH.Core;
+﻿using DH.Core;
+using DH.Core.Infrastructure;
+
 using NewLife.Log;
+
+using NUglify;
+using NUglify.JavaScript;
+
+using WebOptimizer;
+
 using XCode.Membership;
 
-namespace DH.Web.Framework.WebOptimizer.Processors
-{
+namespace DH.Web.Framework.WebOptimizer.Processors;
+
+/// <summary>
+/// 表示处理javascript资产的处理器类
+/// </summary>
+/// <remarks>已从WebOptimizer实施添加日志记录</remarks>
+public partial class DHJsMinifier : Processor {
+    #region Methods
+
     /// <summary>
-    /// 表示处理javascript资产的处理器类
+    /// 在指定的配置上执行处理器。
     /// </summary>
-    /// <remarks>已从WebOptimizer中执行以添加日志记录</remarks>
-    public partial class DHJsMinifier : Processor
-    {
-        #region Methods
+    /// <param name="context">用于对WebOptimizer.IAsset实例执行处理的上下文</param>
+    public override async Task ExecuteAsync(IAssetContext context) {
+        var content = new Dictionary<string, byte[]>();
 
-        /// <summary>
-        /// 在指定的配置上执行处理器。
-        /// </summary>
-        /// <param name="context">用于执行WebOptimizer处理的上下文。IAsset实例</param>
-        public override async Task ExecuteAsync(IAssetContext context)
-        {
-            var content = new Dictionary<string, byte[]>();
-
-            foreach (var key in context.Content.Keys)
-            {
-                if (key.EndsWith(".min"))
-                {
-                    content[key] = context.Content[key];
-                    continue;
-                }
-
-                var input = context.Content[key].AsString();
-                var result = Uglify.Js(input, new CodeSettings { TermSemicolons = true });
-
-                var minified = result.Code;
-
-                if (result.HasErrors)
-                {
-                    var webHelper = EngineContext.Current.Resolve<IWebHelper>();
-                    var workContext = EngineContext.Current.Resolve<IWorkContext>();
-
-                    // 获取当前客户
-                    var currentCustomer = workContext.GetCurrentCustomer();
-
-                    var message = $"JavaScript minification: {key}" + new Exception(string.Join(Environment.NewLine, result.Errors));
-                    XTrace.Log.Error(message);
-                    LogProvider.Provider?.WriteLog("系统", "错误", false, message, currentCustomer.User.ID, currentCustomer.User.Name, webHelper.GetCurrentIpAddress());
-                }
-
-                content[key] = minified.AsByteArray();
+        foreach (var key in context.Content.Keys) {
+            if (key.EndsWith(".min")) {
+                content[key] = context.Content[key];
+                continue;
             }
 
-            context.Content = content;
+            var input = context.Content[key].AsString();
+            var result = Uglify.Js(input, new CodeSettings { TermSemicolons = true });
 
-            await Task.CompletedTask;
-            return ;
+            var minified = result.Code;
+
+            if (result.HasErrors) {
+                var webHelper = EngineContext.Current.Resolve<IWebHelper>();
+                var workContext = EngineContext.Current.Resolve<IWorkContext>();
+
+                // 获取当前客户
+                var currentCustomer = workContext.GetCurrentCustomer();
+
+                var message = $"JavaScript minification: {key}" + new Exception(string.Join(Environment.NewLine, result.Errors));
+                XTrace.Log.Error(message);
+                LogProvider.Provider?.WriteLog("系统", "错误", false, message, currentCustomer.User.ID, currentCustomer.User.Name, webHelper.GetCurrentIpAddress());
+            }
+
+            content[key] = minified.AsByteArray();
         }
 
-        #endregion
+        context.Content = content;
 
+        await Task.CompletedTask;
     }
+
+    #endregion
+
 }
