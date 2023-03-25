93e965d328779ccf5bc6d8deb70413b94b68a4ee
猿人易 <qcjxberin@outlook.com>
2023-03-25 09:56:44 +0800

    增加单点登录消费者事件

---
+++ src/DH.Core/Model/EventModel/ThirdLoginInfoEvent.cs
@@ -0,0 +1,25 @@
+﻿namespace DH.Model.EventModel;
+
+/// <summary>
+/// 单点登录消费者事件
+/// </summary>
+public class ThirdLoginInfoEvent {
+    public ThirdLoginInfoEvent(Int64 userId, Int32 ucId, String openId, string provider, string unionID)
+    {
+        UserId = userId;
+        UcId = ucId;
+        OpenId = openId;
+        Provider = provider;
+        UnionID = unionID;
+    }
+
+    public Int64 UserId { get; set; }
+
+    public Int32 UcId { get; set; }
+
+    public String OpenId { get; set; }
+
+    public String UnionID { get; set; }
+
+    public String Provider { get; set; }
+}
