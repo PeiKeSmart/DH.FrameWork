325049ad9a94006aa641d07006eda74b38e92df0
川流云 <qcjxberin@outlook.com>
2023-04-01 09:57:44 +0800

    feat(microapp): 新增担保支付账单相关接口 test(microapp): 补充担保支付请求签名的单元测试用例 feat(microapp): 新增直播小玩法相关接口

--- src/DH.SKIT.FlurlHttpClient.ByteDance.MicroApp/Extensions/ByteDanceMicroAppClientExecuteAppsECPayExtensions.cs
+++ src/DH.SKIT.FlurlHttpClient.ByteDance.MicroApp/Extensions/ByteDanceMicroAppClientExecuteAppsECPayExtensions.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
@@ -18,22 +18,22 @@ namespace SKIT.FlurlHttpClient.ByteDance.MicroApp
             if (request == null) throw new ArgumentNullException(nameof(request));
 
             /*
-             * REF: https://developer.open-douyin.com/docs/resource/zh-CN/mini-app/develop/server/ecpay/appendix#%E8%AF%B7%E6%B1%82%E7%AD%BE%E5%90%8D%E7%AE%97%E6%B3%95
+             * REF: https://microapp.bytedance.com/docs/zh-CN/mini-app/develop/server/ecpay/TE/#%E4%B8%89%E3%80%81%E7%AD%BE%E5%90%8D-demo
              */
 
             string json = client.JsonSerializer.Serialize(request);
             IList<string> tempList = new List<string>() { client.Credentials.ECPaySalt! };
             IDictionary<string, string> paramMap = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, string>>(json)!;
 
-            foreach (var item in paramMap)
+            foreach (KeyValuePair<string, string> item in paramMap)
             {
                 string key = item.Key;
                 string? value = item.Value?.Trim();
 
-                if ("other_settle_params".Equals(key) ||
-                    "app_id".Equals(key) ||
+                if ("app_id".Equals(key) ||
                     "thirdparty_id".Equals(key) ||
-                    "sign".Equals(key))
+                    "sign".Equals(key) ||
+                    "other_settle_params".Equals(key))
                 {
                     continue;
                 }
@@ -51,8 +51,9 @@ namespace SKIT.FlurlHttpClient.ByteDance.MicroApp
                 tempList.Add(value!);
             }
 
-            var tempArray = tempList.ToArray();
+            string[] tempArray = tempList.ToArray();
             Array.Sort(tempArray, StringComparer.Ordinal);
+
             string plainText = string.Join("&", tempArray);
             return Utilities.MD5Utility.Hash(plainText).ToLower();
         }
@@ -61,7 +62,7 @@ namespace SKIT.FlurlHttpClient.ByteDance.MicroApp
         #region SaaS - Apply
         /// <summary>
         /// <para>异步调用 [POST] /apps/ecpay/saas/create_merchant 接口。</para>
-        /// <para>REF: https://developer.open-douyin.com/docs/resource/zh-CN/mini-app/develop/server/ecpay/APIlist/apply/create </para>
+        /// <para>REF: https://developer.open-douyin.com/docs/resource/zh-CN/mini-app/develop/server/ecpay/apply/create </para>
         /// </summary>
         /// <param name="client"></param>
         /// <param name="request"></param>
@@ -83,7 +84,7 @@ namespace SKIT.FlurlHttpClient.ByteDance.MicroApp
 
         /// <summary>
         /// <para>异步调用 [POST] /apps/ecpay/saas/image_upload 接口。</para>
-        /// <para>REF: https://developer.open-douyin.com/docs/resource/zh-CN/mini-app/develop/server/ecpay/APIlist/apply/image-upload </para>
+        /// <para>REF: https://developer.open-douyin.com/docs/resource/zh-CN/mini-app/develop/server/ecpay/apply/image-upload </para>
         /// </summary>
         /// <param name="client"></param>
         /// <param name="request"></param>
@@ -102,7 +103,7 @@ namespace SKIT.FlurlHttpClient.ByteDance.MicroApp
 
         /// <summary>
         /// <para>异步调用 [POST] /apps/ecpay/saas/query_merchant_status 接口。</para>
-        /// <para>REF: https://developer.open-douyin.com/docs/resource/zh-CN/mini-app/develop/server/ecpay/APIlist/apply/query </para>
+        /// <para>REF: https://developer.open-douyin.com/docs/resource/zh-CN/mini-app/develop/server/ecpay/apply/query </para>
         /// </summary>
         /// <param name="client"></param>
         /// <param name="request"></param>
@@ -226,7 +227,7 @@ namespace SKIT.FlurlHttpClient.ByteDance.MicroApp
         #region SaaS - Withdraw
         /// <summary>
         /// <para>异步调用 [POST] /apps/ecpay/saas/query_merchant_balance 接口。</para>
-        /// <para>REF: https://developer.open-douyin.com/docs/resource/zh-CN/mini-app/develop/server/ecpay/APIlist/withdraw/query-balance </para>
+        /// <para>REF: https://developer.open-douyin.com/docs/resource/zh-CN/mini-app/develop/server/ecpay/withdraw/query-balance </para>
         /// </summary>
         /// <param name="client"></param>
         /// <param name="request"></param>
@@ -248,7 +249,7 @@ namespace SKIT.FlurlHttpClient.ByteDance.MicroApp
 
         /// <summary>
         /// <para>异步调用 [POST] /apps/ecpay/saas/merchant_withdraw 接口。</para>
-        /// <para>REF: https://developer.open-douyin.com/docs/resource/zh-CN/mini-app/develop/server/ecpay/APIlist/withdraw/merchant-withdraw </para>
+        /// <para>REF: https://developer.open-douyin.com/docs/resource/zh-CN/mini-app/develop/server/ecpay/withdraw/merchant-withdraw </para>
         /// </summary>
         /// <param name="client"></param>
         /// <param name="request"></param>
@@ -270,7 +271,7 @@ namespace SKIT.FlurlHttpClient.ByteDance.MicroApp
 
         /// <summary>
         /// <para>异步调用 [POST] /apps/ecpay/saas/query_withdraw_order 接口。</para>
-        /// <para>REF: https://developer.open-douyin.com/docs/resource/zh-CN/mini-app/develop/server/ecpay/APIlist/withdraw/query </para>
+        /// <para>REF: https://developer.open-douyin.com/docs/resource/zh-CN/mini-app/develop/server/ecpay/withdraw/query </para>
         /// </summary>
         /// <param name="client"></param>
         /// <param name="request"></param>
@@ -295,7 +296,7 @@ namespace SKIT.FlurlHttpClient.ByteDance.MicroApp
         #region Order
         /// <summary>
         /// <para>异步调用 [POST] /apps/ecpay/v1/create_order 接口。</para>
-        /// <para>REF: https://developer.open-douyin.com/docs/resource/zh-CN/mini-app/develop/server/ecpay/APIlist/pay-list/pay </para>
+        /// <para>REF: https://developer.open-douyin.com/docs/resource/zh-CN/mini-app/develop/server/ecpay/pay-list/pay </para>
         /// </summary>
         /// <param name="client"></param>
         /// <param name="request"></param>
@@ -320,7 +321,7 @@ namespace SKIT.FlurlHttpClient.ByteDance.MicroApp
 
         /// <summary>
         /// <para>异步调用 [POST] /apps/ecpay/v1/query_order 接口。</para>
-        /// <para>REF: https://developer.open-douyin.com/docs/resource/zh-CN/mini-app/develop/server/ecpay/APIlist/pay-list/query </para>
+        /// <para>REF: https://developer.open-douyin.com/docs/resource/zh-CN/mini-app/develop/server/ecpay/pay-list/query </para>
         /// </summary>
         /// <param name="client"></param>
         /// <param name="request"></param>
@@ -345,7 +346,7 @@ namespace SKIT.FlurlHttpClient.ByteDance.MicroApp
 
         /// <summary>
         /// <para>异步调用 [POST] /apps/ecpay/v1/create_refund 接口。</para>
-        /// <para>REF: https://developer.open-douyin.com/docs/resource/zh-CN/mini-app/develop/server/ecpay/APIlist/refund-list/refund </para>
+        /// <para>REF: https://developer.open-douyin.com/docs/resource/zh-CN/mini-app/develop/server/ecpay/refund-list/refund </para>
         /// </summary>
         /// <param name="client"></param>
         /// <param name="request"></param>
@@ -370,7 +371,7 @@ namespace SKIT.FlurlHttpClient.ByteDance.MicroApp
 
         /// <summary>
         /// <para>异步调用 [POST] /apps/ecpay/v1/query_refund 接口。</para>
-        /// <para>REF: https://developer.open-douyin.com/docs/resource/zh-CN/mini-app/develop/server/ecpay/APIlist/refund-list/query </para>
+        /// <para>REF: https://developer.open-douyin.com/docs/resource/zh-CN/mini-app/develop/server/ecpay/refund-list/query </para>
         /// </summary>
         /// <param name="client"></param>
         /// <param name="request"></param>
@@ -395,7 +396,7 @@ namespace SKIT.FlurlHttpClient.ByteDance.MicroApp
 
         /// <summary>
         /// <para>异步调用 [POST] /apps/ecpay/v1/settle 接口。</para>
-        /// <para>REF: https://developer.open-douyin.com/docs/resource/zh-CN/mini-app/develop/server/ecpay/APIlist/settlements/settlement </para>
+        /// <para>REF: https://developer.open-douyin.com/docs/resource/zh-CN/mini-app/develop/server/ecpay/settlements/settlement </para>
         /// </summary>
         /// <param name="client"></param>
         /// <param name="request"></param>
@@ -420,7 +421,7 @@ namespace SKIT.FlurlHttpClient.ByteDance.MicroApp
 
         /// <summary>
         /// <para>异步调用 [POST] /apps/ecpay/v1/query_settle 接口。</para>
-        /// <para>REF: https://developer.open-douyin.com/docs/resource/zh-CN/mini-app/develop/server/ecpay/APIlist/settlements/query </para>
+        /// <para>REF: https://developer.open-douyin.com/docs/resource/zh-CN/mini-app/develop/server/ecpay/settlements/query </para>
         /// </summary>
         /// <param name="client"></param>
         /// <param name="request"></param>
@@ -445,7 +446,7 @@ namespace SKIT.FlurlHttpClient.ByteDance.MicroApp
 
         /// <summary>
         /// <para>异步调用 [POST] /apps/ecpay/v1/create_return 接口。</para>
-        /// <para>REF: https://developer.open-douyin.com/docs/resource/zh-CN/mini-app/develop/server/ecpay/APIlist/return-settle/ReturnSettle </para>
+        /// <para>REF: https://developer.open-douyin.com/docs/resource/zh-CN/mini-app/develop/server/ecpay/return-settle/ReturnSettle </para>
         /// </summary>
         /// <param name="client"></param>
         /// <param name="request"></param>
@@ -470,7 +471,7 @@ namespace SKIT.FlurlHttpClient.ByteDance.MicroApp
 
         /// <summary>
         /// <para>异步调用 [POST] /apps/ecpay/v1/query_return 接口。</para>
-        /// <para>REF: https://developer.open-douyin.com/docs/resource/zh-CN/mini-app/develop/server/ecpay/APIlist/return-settle/query </para>
+        /// <para>REF: https://developer.open-douyin.com/docs/resource/zh-CN/mini-app/develop/server/ecpay/return-settle/query </para>
         /// </summary>
         /// <param name="client"></param>
         /// <param name="request"></param>
@@ -495,14 +496,14 @@ namespace SKIT.FlurlHttpClient.ByteDance.MicroApp
         #endregion
 
         /// <summary>
-        /// <para>异步调用 [GET] /apps/bill 接口。</para>
-        /// <para>REF: https://developer.open-douyin.com/docs/resource/zh-CN/mini-app/develop/server/ecpay/bill </para>
+        /// <para>异步调用 [GET] /apps/bills 接口。</para>
+        /// <para>REF: https://developer.open-douyin.com/docs/resource/zh-CN/mini-app/develop/server/ecpay/bills/bill </para>
         /// </summary>
         /// <param name="client"></param>
         /// <param name="request"></param>
         /// <param name="cancellationToken"></param>
         /// <returns></returns>
-        public static async Task<Models.AppsBillResponse> ExecuteAppsBillAsync(this ByteDanceMicroAppClient client, Models.AppsBillRequest request, CancellationToken cancellationToken = default)
+        public static async Task<Models.AppsBillsResponse> ExecuteAppsBillsAsync(this ByteDanceMicroAppClient client, Models.AppsBillsRequest request, CancellationToken cancellationToken = default)
         {
             if (client is null) throw new ArgumentNullException(nameof(client));
             if (request is null) throw new ArgumentNullException(nameof(request));
@@ -514,15 +515,52 @@ namespace SKIT.FlurlHttpClient.ByteDance.MicroApp
                 request.Signature = client.GenerateRequestSignature(request);
 
             IFlurlRequest flurlReq = client
-                .CreateRequest(request, HttpMethod.Get, "apps", "bill")
+                .CreateRequest(request, HttpMethod.Get, "apps", "bills")
                 .SetQueryParam("app_id", request.AppId)
-                .SetQueryParam("seller", request.MerchantId)
-                .SetQueryParam("start_date", request.StartDateString)
-                .SetQueryParam("end_date", request.EndDateString)
+                .SetQueryParam("merchant_id", request.MerchantId)
+                .SetQueryParam("bill_date", request.DateString)
                 .SetQueryParam("bill_type", request.BillType)
                 .SetQueryParam("sign", request.Signature);
 
-            return await client.SendRequestWithJsonAsync<Models.AppsBillResponse>(flurlReq, data: request, cancellationToken: cancellationToken);
+            if (request.ComponentAppId != null)
+                flurlReq.SetQueryParam("thirdparty_id", request.ComponentAppId);
+
+            return await client.SendRequestWithJsonAsync<Models.AppsBillsResponse>(flurlReq, data: request, cancellationToken: cancellationToken);
+        }
+
+        /// <summary>
+        /// <para>异步调用 [GET] /apps/fund/bills 接口。</para>
+        /// <para>REF: https://developer.open-douyin.com/docs/resource/zh-CN/mini-app/develop/server/ecpay/bills/fundbill </para>
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task<Models.AppsFundBillsResponse> ExecuteAppsFundBillsAsync(this ByteDanceMicroAppClient client, Models.AppsFundBillsRequest request, CancellationToken cancellationToken = default)
+        {
+            if (client is null) throw new ArgumentNullException(nameof(client));
+            if (request is null) throw new ArgumentNullException(nameof(request));
+
+            if (request.AppId == null)
+                request.AppId = client.Credentials.AppId;
+
+            if (request.Signature == null)
+                request.Signature = client.GenerateRequestSignature(request);
+
+            IFlurlRequest flurlReq = client
+                .CreateRequest(request, HttpMethod.Get, "apps", "fund", "bills")
+                .SetQueryParam("app_id", request.AppId)
+                .SetQueryParam("merchant_id", request.MerchantId)
+                .SetQueryParam("bill_date", request.DateString)
+                .SetQueryParam("account_type", request.AccountType)
+                .SetQueryParam("payment_type", request.PaymentType)
+                .SetQueryParam("trade_type", request.TradeType)
+                .SetQueryParam("sign", request.Signature);
+
+            if (request.ComponentAppId != null)
+                flurlReq.SetQueryParam("thirdparty_id", request.ComponentAppId);
+
+            return await client.SendRequestWithJsonAsync<Models.AppsFundBillsResponse>(flurlReq, data: request, cancellationToken: cancellationToken);
         }
     }
 }
