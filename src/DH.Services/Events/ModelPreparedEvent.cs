ea177f11117394e314827c6c97f2c77d37a59ad5
猿人易 <qcjxberin@outlook.com>
2023-04-19 15:49:28 +0800

    调整Event事件推送

---
+++ src/DH.Services/Events/ModelPreparedEvent.cs
@@ -0,0 +1,29 @@
+﻿namespace DH.Services.Events;
+
+/// <summary>
+/// 表示模型准备查看后发生的事件
+/// </summary>
+/// <typeparam name="T">模型的类型</typeparam>
+public partial class ModelPreparedEvent<T> {
+    #region Ctor
+
+    /// <summary>
+    /// Ctor
+    /// </summary>
+    /// <param name="model">Model</param>
+    public ModelPreparedEvent(T model)
+    {
+        Model = model;
+    }
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// Gets a model
+    /// </summary>
+    public T Model { get; private set; }
+
+    #endregion
+}
