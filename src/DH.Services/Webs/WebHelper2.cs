3f1af35824688afb01d04f385db87aa7fb28420d
川流云 <qcjxberin@outlook.com>
2023-07-11 22:43:03 +0800

    清理方法

--- src/DH.Services/Webs/WebHelper2.cs
+++ src/DH.Services/Webs/WebHelper2.cs
@@ -556,49 +556,4 @@ public static class WebHelper2
 
         return request.Headers["X-Requested-With"] == "XMLHttpRequest";
     }
-
-    /// <summary>获取原始请求Url，支持反向代理</summary>
-    /// <param name="request"></param>
-    /// <returns></returns>
-    public static Uri GetRawUrl(this HttpRequest request)
-    {
-        Uri uri = null;
-
-        //// 配置
-        //var ms = OAuthConfig.GetValids();
-        //var mi = ms.FirstOrDefault(e => !e.AppUrl.IsNullOrEmpty());
-        //if (mi != null) uri = new Uri(mi.AppUrl);
-
-        // 取请求头
-        if (uri == null)
-        {
-            var url = request.GetEncodedUrl();
-            uri = new Uri(url);
-        }
-
-        return GetRawUrl(uri, k => request.Headers[k]);
-    }
-
-    private static Uri GetRawUrl(Uri uri, Func<String, String> headers)
-    {
-        var str = headers("HTTP_X_REQUEST_URI");
-        if (str.IsNullOrEmpty()) str = headers("X-Request-Uri");
-
-        if (str.IsNullOrEmpty())
-        {
-            // 阿里云CDN默认支持 X-Client-Scheme: https
-            var scheme = headers("HTTP_X_CLIENT_SCHEME");
-            if (scheme.IsNullOrEmpty()) scheme = headers("X-Client-Scheme");
-
-            // nginx
-            if (scheme.IsNullOrEmpty()) scheme = headers("HTTP_X_FORWARDED_PROTO");
-            if (scheme.IsNullOrEmpty()) scheme = headers("X-Forwarded-Proto");
-
-            if (!scheme.IsNullOrEmpty()) str = scheme + "://" + uri.ToString().Substring("://");
-        }
-
-        if (!str.IsNullOrEmpty()) uri = new Uri(uri, str);
-
-        return uri;
-    }
 }
