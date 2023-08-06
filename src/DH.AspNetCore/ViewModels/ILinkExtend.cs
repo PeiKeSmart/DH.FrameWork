07177e6d207ea7a6078cfc7df4ad661cee83a009
猿人易 <qcjxberin@outlook.com>
2023-08-06 16:02:23 +0800

    feat: 新增ILinkExtend，支持对列表页数据列的高级自定义行为

---
+++ src/DH.AspNetCore/ViewModels/ILinkExtend.cs
@@ -0,0 +1,12 @@
+﻿using NewLife.Data;
+
+namespace DH.ViewModels;
+
+/// <summary>超链接扩展</summary>
+public interface ILinkExtend {
+    /// <summary>解析超链接HTML</summary>
+    /// <param name="field"></param>
+    /// <param name="data"></param>
+    /// <returns></returns>
+    String Resolve(DataField field, IModel data);
+}
