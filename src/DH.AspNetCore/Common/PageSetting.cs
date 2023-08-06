e91d076ac04fcfc6e2eee810130ae62e9618c6e9
猿人易 <qcjxberin@outlook.com>
2023-08-06 15:28:37 +0800

    解析StarWeb服务时，加上埋点，并捕获异常

---
+++ src/DH.AspNetCore/Common/PageSetting.cs
@@ -0,0 +1,47 @@
+﻿namespace DH;
+
+/// <summary>页面设置。用于控制各页面功能</summary>
+public class PageSetting {
+    #region 静态
+    /// <summary>全局设置</summary>
+    public static PageSetting Global { get; } = new PageSetting();
+    #endregion
+
+    #region 属性
+    /// <summary>导航条所对应的视图。用于多页面共用导航条</summary>
+    public String NavView { get; set; }
+
+    /// <summary>启用导航栏。默认true</summary>
+    public Boolean EnableNavbar { get; set; } = true;
+
+    /// <summary>启用工具栏。默认true</summary>
+    public Boolean EnableToolbar { get; set; } = true;
+
+    /// <summary>启用添加按钮。默认true</summary>
+    public Boolean EnableAdd { get; set; } = true;
+
+    /// <summary>启用关键字搜索。默认true</summary>
+    public Boolean EnableKey { get; set; } = true;
+
+    /// <summary>启用选择列。默认true</summary>
+    public Boolean EnableSelect { get; set; } = true;
+
+    /// <summary>启用尾部。默认true</summary>
+    public Boolean EnableFooter { get; set; } = true;
+
+    /// <summary>是否只读页面。只读页面没有添加编辑删除等按钮</summary>
+    public Boolean IsReadOnly { get; set; }
+
+    /// <summary>启用列表双击事件配置</summary>
+    public Boolean EnableTableDoubleClick { get; set; } = true;
+    #endregion
+
+    #region 构造
+    #endregion
+
+    #region 方法
+    /// <summary>克隆</summary>
+    /// <returns></returns>
+    public PageSetting Clone() => MemberwiseClone() as PageSetting;
+    #endregion
+}
