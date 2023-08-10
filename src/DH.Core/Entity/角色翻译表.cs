ad2f48a53e7a0aced1949bcac8c00bb2425b9678
猿人易 <qcjxberin@outlook.com>
2023-08-10 11:42:02 +0800

    调整字段长度

--- src/DH.Core/Entity/角色翻译表.cs
+++ src/DH.Core/Entity/角色翻译表.cs
@@ -58,7 +58,7 @@ public partial class RoleLan : IRoleLan, IEntity<RoleLanModel>
     /// <summary>备注</summary>
     [DisplayName("备注")]
     [Description("备注")]
-    [DataObjectField(false, false, true, 50)]
+    [DataObjectField(false, false, true, 200)]
     [BindColumn("Remark", "备注", "")]
     public String Remark { get => _Remark; set { if (OnPropertyChanging("Remark", value)) { _Remark = value; OnPropertyChanged("Remark"); } } }
     #endregion
