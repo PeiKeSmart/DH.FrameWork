648f08aa966b8da9c0bb00c3604570a36013018b
猿人易 <qcjxberin@outlook.com>
2023-11-02 22:57:32 +0800

    设备统计默认显示操作系统的今天占比，按30天活跃降序 本地应用看门狗测试通过

--- src/DH.NStardust/LocalStarClient.cs
+++ src/DH.NStardust/LocalStarClient.cs
@@ -2,7 +2,6 @@
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Net;
-using System.Net.Http;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using NewLife;
