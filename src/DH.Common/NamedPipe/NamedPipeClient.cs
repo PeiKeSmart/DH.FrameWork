26825e732b4da8e80f93fa7281ac6b0069697582
猿人易 <qcjxberin@outlook.com>
2023-11-15 16:36:17 +0800

    增加管理通信客户端的主动断开方法

--- src/DH.Common/NamedPipe/NamedPipeClient.cs
+++ src/DH.Common/NamedPipe/NamedPipeClient.cs
@@ -1,4 +1,7 @@
-﻿using System.IO.Pipes;
+﻿using NewLife;
+using NewLife.Log;
+
+using System.IO.Pipes;
 using System.Text;
 
 namespace DH.NamedPipe;
@@ -24,42 +27,51 @@ public class NamedPipeClient {
 
     public string Request(string outPutStr)
     {
-        this.OutputStr = outPutStr.Trim();
-        this.Client.Connect();
-
-        for (int i = 0; i < this.BufferSize; i++)
-            this.OutputStr = " " + this.OutputStr;
-        using (MemoryStream memoryStream = new MemoryStream())
-        using (StreamWriter writer = new StreamWriter(memoryStream))
+        try
         {
+            this.OutputStr = outPutStr.Trim();
+            this.Client.Connect();
 
-            writer.Write(OutputStr);
-            writer.Flush();
-            memoryStream.Flush();
-            int length = 0;
-            memoryStream.Position = 0;
-            while ((length = memoryStream.Read(Buffer, 0, Buffer.Length)) != 0)
+            for (int i = 0; i < this.BufferSize; i++)
+                this.OutputStr = " " + this.OutputStr;
+            using (MemoryStream memoryStream = new MemoryStream())
+            using (StreamWriter writer = new StreamWriter(memoryStream))
             {
-                this.Client.Write(Buffer, 0, length);
+
+                writer.Write(OutputStr);
+                writer.Flush();
+                memoryStream.Flush();
+                int length = 0;
+                memoryStream.Position = 0;
+                while ((length = memoryStream.Read(Buffer, 0, Buffer.Length)) != 0)
+                {
+                    this.Client.Write(Buffer, 0, length);
+                }
+                this.Client.WaitForPipeDrain();
+                this.Client.Flush();
             }
-            this.Client.WaitForPipeDrain();
-            this.Client.Flush();
+
+            AsyncState asyncState = new AsyncState()
+            {
+                Buffer = new byte[BufferSize],
+                EvtHandle = new ManualResetEvent(false),
+                Stream = this.Client
+            };
+
+            IAsyncResult readAsyncResult = this.Client.BeginRead(this.Buffer, 0, this.Buffer.Length, new AsyncCallback(ReadCallback), asyncState);
+            asyncState.EvtHandle.WaitOne();
+
+            this.Client.Close();
+            this.Client.Dispose();
+
+            return this.InputStr.ToString().Replace("\0", " ").Trim();
+        }
+        catch(Exception ex)
+        {
+            XTrace.WriteException(ex);
         }
 
-        AsyncState asyncState = new AsyncState()
-        {
-            Buffer = new byte[BufferSize],
-            EvtHandle = new ManualResetEvent(false),
-            Stream = this.Client
-        };
-
-        IAsyncResult readAsyncResult = this.Client.BeginRead(this.Buffer, 0, this.Buffer.Length, new AsyncCallback(ReadCallback), asyncState);
-        asyncState.EvtHandle.WaitOne();
-
-        this.Client.Close();
-        this.Client.Dispose();
-
-        return this.InputStr.ToString().Replace("\0", " ").Trim();
+        return "出错了";
     }
 
     private void ReadCallback(IAsyncResult arg)
@@ -100,5 +112,15 @@ public class NamedPipeClient {
             //再次执行异步读取操作
             state.Stream.BeginRead(state.Buffer, 0, BufferSize, new AsyncCallback(ReadCallback), state);
         }
+    }
+
+    /// <summary>
+    /// 断开连接
+    /// </summary>
+    public void Close()
+    {
+        Client.Close();
+        Client.TryDispose();
+        Client = null;
     }
 }
