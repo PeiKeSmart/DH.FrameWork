4fb945c33f126ec10c91e7f0daac047b37866946
猿人易 <qcjxberin@outlook.com>
2023-11-17 10:48:23 +0800

    增加判断端口是否被占用类

---
+++ src/DH.Common/Helpers/NetHelper.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using System.Net.NetworkInformation;
+
+namespace DH.Helpers;
+
+/// <summary>
+/// 网络端帮助类
+/// </summary>
+public static class NetHelper {
+
+    /// <summary>
+    /// 判断端口是否被占用
+    /// </summary>
+    /// <param name="port"></param>
+    /// <returns></returns>
+    public static bool PortInUse(int port)
+    {
+        bool inUse = false;
+
+        IPGlobalProperties ipProperties = IPGlobalProperties.GetIPGlobalProperties();
+        IPEndPoint[] ipEndPoints = ipProperties.GetActiveTcpListeners();
+
+        foreach (IPEndPoint endPoint in ipEndPoints)
+        {
+            if (endPoint.Port == port)
+            {
+                inUse = true;
+                break;
+            }
+        }
+
+        return inUse;
+    }
+
+}
