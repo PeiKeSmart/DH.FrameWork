2c7a2c7f8ab9f4c5e0986ab1a5e40832ea4ce726
猿人易 <qcjxberin@outlook.com>
2023-11-28 16:14:07 +0800

    修复配置文件

--- src/DH.WebHook/WebHookSetting.cs
+++ src/DH.WebHook/WebHookSetting.cs
@@ -1,24 +1,24 @@
-﻿using NewLife.Xml;
-using System;
+﻿using NewLife.Configuration;
+
 using System.ComponentModel;
 
-namespace DH.WebHook
-{
-    /// <summary>WebHooks设置</summary>
-    [DisplayName("WebHooks设置")]
-    [XmlConfigFile("Config/WebHookSetting.config", 10000)]
-    public class WebHookSetting : XmlConfig<WebHookSetting>
-    {
-        /// <summary>钉钉Host</summary>
-        [Description("钉钉Host")]
-        public String DingTalkHost { get; set; }
+namespace DH.WebHook;
 
-        /// <summary>钉钉机器人接口地址</summary>
-        [Description("钉钉机器人接口地址")]
-        public String DingTalkSendUrl { get; set; }
+/// <summary>WebHooks设置</summary>
+[DisplayName("WebHooks设置")]
+//[XmlConfigFile("Config/WebHookSetting.config", 10000)]
+[Config("WebHookSetting")]
+//public class WebHookSetting : XmlConfig<WebHookSetting>
+public class WebHookSetting : Config<WebHookSetting> {
+    /// <summary>钉钉Host</summary>
+    [Description("钉钉Host")]
+    public String DingTalkHost { get; set; }
 
-        /// <summary>企业微信机器人密钥</summary>
-        [Description("企业微信机器人密钥")]
-        public String WeChatWorkAppId { get; set; }
-    }
+    /// <summary>钉钉机器人接口地址</summary>
+    [Description("钉钉机器人接口地址")]
+    public String DingTalkSendUrl { get; set; }
+
+    /// <summary>企业微信机器人密钥</summary>
+    [Description("企业微信机器人密钥")]
+    public String WeChatWorkAppId { get; set; }
 }
