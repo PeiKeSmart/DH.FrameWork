27f993c8ca67a105dd68a2555e0254274df9da50
猿人易 <qcjxberin@outlook.com>
2024-03-25 16:42:41 +0800

    增加极光推送库

---
+++ src/DH.JPush/JPushClient.cs
@@ -0,0 +1,248 @@
+﻿using Jiguang.JPush.Model;
+
+using NewLife;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Jiguang.JPush;
+
+public class JPushClient
+{
+    public const string BASE_URL_PUSH_DEFAULT = "https://api.jpush.cn/v3/push";
+    public const string BASE_URL_PUSH_BEIJING = "https://bjapi.push.jiguang.cn/v3/push";
+
+    private string BASE_URL = BASE_URL_PUSH_DEFAULT;
+
+    public DeviceClient Device;
+    public ScheduleClient Schedule;
+    private ReportClient report;
+
+    public ReportClient Report { get => report; set => report = value; }
+
+    public static HttpClient HttpClient;
+
+    static JPushClient()
+    {
+        if (Runtime.Linux)
+        {
+            var httpClientHandler = new HttpClientHandler
+            {
+                ServerCertificateCustomValidationCallback = (message, certificate2, arg3, arg4) => true
+            };
+            HttpClient = new HttpClient(httpClientHandler);
+        }
+        else
+        {
+            HttpClient = new HttpClient();
+        }
+        HttpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+    }
+
+    public JPushClient(string appKey, string masterSecret)
+    {
+        if (string.IsNullOrEmpty(appKey))
+            throw new ArgumentNullException(nameof(appKey));
+
+        if (string.IsNullOrEmpty(masterSecret))
+            throw new ArgumentNullException(nameof(masterSecret));
+
+        var auth = Convert.ToBase64String(Encoding.UTF8.GetBytes(appKey + ":" + masterSecret));
+        HttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", auth);
+
+        Report = new ReportClient();
+        Device = new DeviceClient();
+        Schedule = new ScheduleClient();
+    }
+
+    /// <summary>
+    /// 设置 push 功能的 API 调用地址。
+    /// <para>
+    /// 如果极光应用分配在北京机房（极光控制台 “应用设置” -> “应用信息” 中可以看到），并且开发者接口调用的服务器也位于北京，则可以调用如下地址：
+    ///
+    /// https://bjapi.push.jiguang.cn/v3/push
+    /// <para>可以提升 API 的响应速度。</para>
+    /// </para>
+    /// </summary>
+    /// <param name="url"><see cref="BASE_URL_DEFAULT"/> or <see cref="BASE_URL_BEIJING"/></param>
+    public void SetBaseURL(string url)
+    {
+        BASE_URL = url;
+    }
+
+    public async Task<HttpResponse> SendPushAsync(string jsonBody)
+    {
+        if (string.IsNullOrEmpty(jsonBody))
+            throw new ArgumentNullException(nameof(jsonBody));
+
+        HttpContent httpContent = new StringContent(jsonBody, Encoding.UTF8);
+        HttpResponseMessage msg = await HttpClient.PostAsync(BASE_URL, httpContent).ConfigureAwait(false);
+        var content = await msg.Content.ReadAsStringAsync().ConfigureAwait(false);
+        return new HttpResponse(msg.StatusCode, msg.Headers, content);
+    }
+
+    /// <summary>
+    /// <see cref="SendPush(PushPayload)"/>
+    /// </summary>
+    public async Task<HttpResponse> SendPushAsync(PushPayload payload)
+    {
+        if (payload == null)
+            throw new ArgumentNullException(nameof(payload));
+
+        string body = payload.ToString();
+        return await SendPushAsync(body);
+    }
+
+    /// <summary>
+    /// 进行消息推送。
+    /// <see cref="https://docs.jiguang.cn/jpush/server/push/rest_api_v3_push/#_1"/>
+    /// </summary>
+    /// <param name="pushPayload"> 推送对象。<see cref="https://docs.jiguang.cn/jpush/server/push/rest_api_v3_push/#_7"/> </param>
+    public HttpResponse SendPush(PushPayload pushPayload)
+    {
+        Task<HttpResponse> task = Task.Run(() => SendPushAsync(pushPayload));
+        task.Wait();
+        return task.Result;
+    }
+
+    public async Task<HttpResponse> IsPushValidAsync(string jsonBody)
+    {
+        if (string.IsNullOrEmpty(jsonBody))
+            throw new ArgumentNullException(nameof(jsonBody));
+
+        HttpContent httpContent = new StringContent(jsonBody, Encoding.UTF8);
+        var url = BASE_URL + "/validate";
+        HttpResponseMessage msg = await HttpClient.PostAsync(url, httpContent).ConfigureAwait(false);
+        var content = await msg.Content.ReadAsStringAsync().ConfigureAwait(false);
+        return new HttpResponse(msg.StatusCode, msg.Headers, content);
+    }
+
+    /// <summary>
+    /// <see cref="IsPushValid(PushPayload)"/>
+    /// </summary>
+    public async Task<HttpResponse> IsPushValidAsync(PushPayload payload)
+    {
+        if (payload == null)
+            throw new ArgumentNullException(nameof(payload));
+
+        var body = payload.ToString();
+        return await IsPushValidAsync(body);
+    }
+
+    /// <summary>
+    /// 校验推送能否成功。与推送 API 的区别在于：不会实际向用户发送任何消息。 其他字段说明和推送 API 完全相同。
+    /// </summary>
+    /// <param name="pushPayload"> 推送对象。<see cref="https://docs.jiguang.cn/jpush/server/push/rest_api_v3_push/#_7"/> </param>
+    public HttpResponse IsPushValid(PushPayload pushPayload)
+    {
+        Task<HttpResponse> task = Task.Run(() => IsPushValidAsync(pushPayload));
+        task.Wait();
+        return task.Result;
+    }
+
+    /// <summary>
+    /// <see cref="GetCIdList(int?, string)"/>
+    /// </summary>
+    public async Task<HttpResponse> GetCIdListAsync(int? count, string type)
+    {
+        if (count != null && count < 1 && count > 1000)
+            throw new ArgumentOutOfRangeException(nameof(count));
+
+        var url = BASE_URL + "/cid";
+
+        if (count != null)
+        {
+            url += ("?count=" + count);
+
+            if (!string.IsNullOrEmpty(type))
+                url += ("&type=" + type);
+        }
+
+        HttpResponseMessage msg = await HttpClient.GetAsync(url).ConfigureAwait(false);
+        var content = await msg.Content.ReadAsStringAsync().ConfigureAwait(false);
+        return new HttpResponse(msg.StatusCode, msg.Headers, content);
+    }
+
+    /// <summary>
+    /// 获取 CId（推送的唯一标识符） 列表。
+    /// <see cref="https://docs.jiguang.cn/jpush/server/push/rest_api_v3_push/#cid"/>
+    /// </summary>
+    /// <param name="count">不传默认为 1。范围为[1, 1000]</param>
+    /// <param name="type">CId 的类型。取值："push" (默认) 或 "schedule"</param>
+    public HttpResponse GetCIdList(int? count, string type)
+    {
+        Task<HttpResponse> task = Task.Run(() => GetCIdListAsync(count, type));
+        task.Wait();
+        return task.Result;
+    }
+
+    /// <summary>
+    /// 针对RegID方式批量单推（VIP专属接口）
+    /// 如果您在给每个用户的推送内容都不同的情况下，可以使用此接口。
+    /// <see cref="https://docs.jiguang.cn/jpush/server/push/rest_api_v3_push/#vip"/>
+    /// </summary>
+    /// <param name="singlePayLoadList">批量单推的载体列表</param>
+    public async Task<HttpResponse> BatchPushByRegidAsync(List<SinglePayload> singlePayLoadList)
+    {
+        var url = BASE_URL + "/batch/regid/single";
+        return await BatchPushAsync(url, singlePayLoadList);
+    }
+
+    /// <summary>
+    /// 针对RegID方式批量单推（VIP专属接口）
+    /// 如果您在给每个用户的推送内容都不同的情况下，可以使用此接口。
+    /// <see cref="https://docs.jiguang.cn/jpush/server/push/rest_api_v3_push/#vip"/>
+    /// </summary>
+    /// <param name="singlePayLoadList">批量单推的载体列表</param>
+    public HttpResponse BatchPushByRegid(List<SinglePayload> singlePayLoadList)
+    {
+        Task<HttpResponse> task = Task.Run(() => BatchPushByRegidAsync(singlePayLoadList));
+        task.Wait();
+        return task.Result;
+    }
+
+    /// <summary>
+    /// 针对Alias方式批量单推（VIP专属接口）
+    /// 如果您在给每个用户的推送内容都不同的情况下，可以使用此接口。
+    /// <see cref="https://docs.jiguang.cn/jpush/server/push/rest_api_v3_push/#vip"/>
+    /// </summary>
+    /// <param name="singlePayLoadList">批量单推的载体列表</param>
+    public async Task<HttpResponse> BatchPushByAliasAsync(List<SinglePayload> singlePayLoadList)
+    {
+        var url = BASE_URL + "/batch/alias/single";
+        return await BatchPushAsync(url, singlePayLoadList);
+    }
+
+    /// <summary>
+    /// 针对Alias方式批量单推（VIP专属接口）
+    /// 如果您在给每个用户的推送内容都不同的情况下，可以使用此接口。
+    /// <see cref="https://docs.jiguang.cn/jpush/server/push/rest_api_v3_push/#vip"/>
+    /// </summary>
+    /// <param name="singlePayLoadList">批量单推的载体列表</param>
+    public HttpResponse BatchPushByAlias(List<SinglePayload> singlePayLoadList)
+    {
+        Task<HttpResponse> task = Task.Run(() => BatchPushByAliasAsync(singlePayLoadList));
+        task.Wait();
+        return task.Result;
+    }
+
+    private async Task<HttpResponse> BatchPushAsync(String url, List<SinglePayload> singlePayLoadList)
+    {
+        HttpResponse cidResponse = await this.GetCIdListAsync(singlePayLoadList.Count, "push");
+        JObject jObject = (JObject)JsonConvert.DeserializeObject(cidResponse.Content);
+        JArray jArray = ((JArray)jObject["cidlist"]);
+        BatchPushPayload batchPushPayload = new BatchPushPayload();
+        batchPushPayload.Pushlist = new Dictionary<String, SinglePayload>();
+        for (int i = 0; i < singlePayLoadList.Count; i++)
+        {
+            batchPushPayload.Pushlist.Add((String)jArray[i], singlePayLoadList[i]);
+        }
+        HttpContent httpContent = new StringContent(batchPushPayload.ToString(), Encoding.UTF8);
+        HttpResponseMessage msg = await HttpClient.PostAsync(url, httpContent).ConfigureAwait(false);
+        var content = await msg.Content.ReadAsStringAsync().ConfigureAwait(false);
+        return new HttpResponse(msg.StatusCode, msg.Headers, content);
+    }
+}
---
+++ src/DH.JPush/Model/Notification.cs
@@ -0,0 +1,118 @@
+﻿using Newtonsoft.Json;
+
+namespace Jiguang.JPush.Model;
+
+/// <summary>
+/// <see cref="https://docs.jiguang.cn/jpush/server/push/rest_api_v3_push/#notification"/>
+/// </summary>
+public class Notification
+{
+    [JsonProperty("alert")]
+    public string Alert { get; set; }
+
+    [JsonProperty("android", NullValueHandling = NullValueHandling.Ignore)]
+    public Android Android { get; set; }
+
+    [JsonProperty("ios", NullValueHandling = NullValueHandling.Ignore)]
+    public IOS IOS { get; set; }
+}
+
+public class Android
+{
+    /// <summary>
+    /// 必填。
+    /// </summary>
+    [JsonProperty("alert")]
+    public string Alert { get; set; }
+
+    [JsonProperty("title", NullValueHandling = NullValueHandling.Ignore)]
+    public string Title { get; set; }
+
+    [JsonProperty("builder_id", NullValueHandling = NullValueHandling.Ignore)]
+    public int? BuilderId { get; set; }
+
+    [JsonProperty("channel_id", NullValueHandling = NullValueHandling.Ignore)]
+    public string ChannelId { get; set; }
+
+    [JsonProperty("priority", NullValueHandling = NullValueHandling.Ignore)]
+    public int? Priority { get; set; }
+
+    [JsonProperty("category", NullValueHandling = NullValueHandling.Ignore)]
+    public string Category { get; set; }
+
+    [JsonProperty("style", NullValueHandling = NullValueHandling.Ignore)]
+    public int? Style { get; set; }
+
+    [JsonProperty("alert_type", NullValueHandling = NullValueHandling.Ignore)]
+    public int? AlertType { get; set; }
+
+    [JsonProperty("big_text", NullValueHandling = NullValueHandling.Ignore)]
+    public string BigText { get; set; }
+
+    [JsonProperty("inbox", NullValueHandling = NullValueHandling.Ignore)]
+    public Dictionary<string, object> Inbox { get; set; }
+
+    [JsonProperty("big_pic_path", NullValueHandling = NullValueHandling.Ignore)]
+    public string BigPicturePath { get; set; }
+
+    [JsonProperty("large_icon", NullValueHandling = NullValueHandling.Ignore)]
+    public string LargeIcon { get; set; }
+
+    [JsonProperty("intent", NullValueHandling = NullValueHandling.Ignore)]
+    public Dictionary<string, object> Indent { get; set; }
+
+    [JsonProperty("extras", NullValueHandling = NullValueHandling.Ignore)]
+    public Dictionary<string, object> Extras { get; set; }
+
+    /// <summary>
+    /// (VIP only)指定开发者想要打开的 Activity，值为 <activity> 节点的 "android:name" 属性值。
+    /// </summary>
+    [JsonProperty("uri_activity", NullValueHandling = NullValueHandling.Ignore)]
+    public string URIActivity { get; set; }
+
+    /// <summary>
+    /// (VIP only)指定打开 Activity 的方式，值为 Intent.java 中预定义的 "access flags" 的取值范围。
+    /// </summary>
+    [JsonProperty("uri_flag", NullValueHandling = NullValueHandling.Ignore)]
+    public string URIFlag { get; set; }
+
+    /// <summary>
+    /// (VIP only)指定开发者想要打开的 Activity，值为 <activity> -> <intent-filter> -> <action> 节点中的 "android:name" 属性值。
+    /// </summary>
+    [JsonProperty("uri_action", NullValueHandling = NullValueHandling.Ignore)]
+    public string URIAction { get; set; }
+}
+
+public class IOS
+{
+    /// <summary>
+    /// 可以是 string，也可以是 Apple 官方定义的 alert payload 结构。
+    /// <para><see ="https://developer.apple.com/library/content/documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/PayloadKeyReference.html#//apple_ref/doc/uid/TP40008194-CH17-SW5"/></para>
+    /// </summary>
+    [JsonProperty("alert")]
+    public object Alert { get; set; }
+
+    [JsonProperty("sound", NullValueHandling = NullValueHandling.Ignore)]
+    public string Sound { get; set; }
+
+    /// <summary>
+    /// 默认角标 +1。
+    /// </summary>
+    [JsonProperty("badge")]
+    public string Badge { get; set; } = "+1";
+
+    [JsonProperty("content-available", NullValueHandling = NullValueHandling.Ignore)]
+    public bool? ContentAvailable { get; set; }
+
+    [JsonProperty("mutable-content", NullValueHandling = NullValueHandling.Ignore)]
+    public bool? MutableContent { get; set; }
+
+    [JsonProperty("category", NullValueHandling = NullValueHandling.Ignore)]
+    public string Category { get; set; }
+
+    [JsonProperty("extras", NullValueHandling = NullValueHandling.Ignore)]
+    public Dictionary<string, object> Extras { get; set; }
+
+    [JsonProperty("thread-id", NullValueHandling = NullValueHandling.Ignore)]
+    public string ThreadId { get; set; }
+}
