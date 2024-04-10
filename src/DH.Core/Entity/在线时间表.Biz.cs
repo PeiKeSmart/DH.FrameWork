f169a70dd8bf3683fb47fca9aeeb947b94cdad13
猿人易 <qcjxberin@outlook.com>
2024-04-10 11:50:05 +0800

    清理日志

--- src/DH.Core/Entity/在线时间表.Biz.cs
+++ src/DH.Core/Entity/在线时间表.Biz.cs
@@ -363,8 +363,6 @@ public partial class SysOnlineTime : DHEntityBase<SysOnlineTime> {
         var model = FindByIdAndYearAndMonth(uid, updateTime.Year, updateTime.Month);
         if (model != null)
         {
-            XTrace.WriteLine($"测试写入在线时间表的数据3：{onlineTime}_{model.ToJson()}");
-
             model.UId = uid;
             model.RoleId = roleId;
             model.UName = uName;
@@ -516,8 +514,6 @@ public partial class SysOnlineTime : DHEntityBase<SysOnlineTime> {
 
             //model[$"Day{updateTime.Day}"] = model.DayTimes;
             //model.SetItem($"Day{updateTime.Day}", model.DayTimes);
-
-            XTrace.WriteLine($"测试写入在线时间表的数据2：{onlineTime}_{model.ToJson()}");
 
             model.Update();
         }
