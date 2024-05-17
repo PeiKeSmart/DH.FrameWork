937f47afeff87f2af75fa2320f4778622efbf204
猿人易 <qcjxberin@outlook.com>
2024-05-17 17:23:43 +0800

    增加Hook类库

---
+++ src/DH.DotNetDetour/Extensions/TypeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+namespace DH.DotNetDetour.Extensions;
+
+public static class TypeExtensions {
+    public static T GetCustomAttribute<T>(this MemberInfo @this)
+    {
+        var list = @this.GetCustomAttributes(typeof(T), true)?.ToList();
+        return (T)list.FirstOrDefault();
+    }
+
+    public static T GetCustomAttribute<T>(this ParameterInfo @this)
+    {
+        var list = @this.GetCustomAttributes(typeof(T), true)?.ToList();
+        return (T)list.FirstOrDefault();
+    }
+}
