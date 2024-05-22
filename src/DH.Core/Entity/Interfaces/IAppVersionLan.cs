bd2103299d562b0302eb3166125228634ac0f0be
猿人易 <qcjxberin@outlook.com>
2024-05-22 13:41:11 +0800

    增加App版本翻译表

---
+++ src/DH.Core/Entity/Interfaces/IAppVersionLan.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.Serialization;
+using System.Web.Script.Serialization;
+using System.Xml.Serialization;
+
+namespace DH.Entity;
+
+/// <summary>APP版本翻译</summary>
+public partial interface IAppVersionLan
+{
+    #region 属性
+    /// <summary>编号</summary>
+    Int32 Id { get; set; }
+
+    /// <summary>APP版本Id</summary>
+    Int32 AId { get; set; }
+
+    /// <summary>关联所属语言Id</summary>
+    Int32 LId { get; set; }
+
+    /// <summary>升级内容</summary>
+    String Content { get; set; }
+    #endregion
+}
---
+++ src/DH.Core/Entity/Models/AppVersionLanModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.Serialization;
+using System.Web.Script.Serialization;
+using System.Xml.Serialization;
+
+namespace DH.Entity;
+
+/// <summary>APP版本翻译</summary>
+public partial class AppVersionLanModel
+{
+    #region 属性
+    /// <summary>编号</summary>
+    public Int32 Id { get; set; }
+
+    /// <summary>APP版本Id</summary>
+    public Int32 AId { get; set; }
+
+    /// <summary>关联所属语言Id</summary>
+    public Int32 LId { get; set; }
+
+    /// <summary>升级内容</summary>
+    public String Content { get; set; }
+    #endregion
+
+    #region 拷贝
+    /// <summary>拷贝模型对象</summary>
+    /// <param name="model">模型</param>
+    public void Copy(IAppVersionLan model)
+    {
+        Id = model.Id;
+        AId = model.AId;
+        LId = model.LId;
+        Content = model.Content;
+    }
+    #endregion
+}
