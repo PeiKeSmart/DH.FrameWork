51ab4d7a7d78c0dd7baa28e8805a1d073aaa9850
猿人易 <qcjxberin@outlook.com>
2024-06-11 09:20:46 +0800

    进一步抽象集中客户端 使用UTC时间传输，客户端转本地时间，避免时区差异。 有些序列化框架可能不支持带时区信息的序列化，因此约定使用UTC时间传输。 客户端全部使用接口来替代模型类，支持对象容器的依赖注入

--- src/DH.NRemoting/Models/PingRequest.cs
+++ src/DH.NRemoting/Models/PingRequest.cs
@@ -1,7 +1,20 @@
 ﻿namespace NewLife.Remoting.Models;
 
 /// <summary>心跳请求</summary>
-public class PingRequest
+public interface IPingRequest
+{
+    /// <summary>开机时间，单位s</summary>
+    Int32 Uptime { get; set; }
+
+    /// <summary>本地UTC时间。Unix毫秒（UTC）</summary>
+    Int64 Time { get; set; }
+
+    /// <summary>延迟。请求到服务端并返回的延迟时间。单位ms</summary>
+    Int32 Delay { get; set; }
+}
+
+/// <summary>心跳请求</summary>
+public class PingRequest : IPingRequest
 {
     #region 属性
     /// <summary>可用内存大小</summary>
@@ -61,10 +74,10 @@ public class PingRequest
     /// <summary>开机时间，单位s</summary>
     public Int32 Uptime { get; set; }
 
-    /// <summary>本地UTC时间。ms毫秒</summary>
+    /// <summary>本地UTC时间。Unix毫秒（UTC）</summary>
     public Int64 Time { get; set; }
 
-    /// <summary>延迟</summary>
+    /// <summary>延迟。请求到服务端并返回的延迟时间。单位ms</summary>
     public Int32 Delay { get; set; }
     #endregion
 }
