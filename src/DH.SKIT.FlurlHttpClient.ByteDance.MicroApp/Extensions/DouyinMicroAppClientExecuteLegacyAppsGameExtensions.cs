6fad860ef0649d41ef45a1201c5345535829f462
猿人易 <qcjxberin@outlook.com>
2024-07-15 16:39:33 +0800

    fix(microapp): 修复逻辑错误

--- src/DH.SKIT.FlurlHttpClient.ByteDance.MicroApp/Extensions/DouyinMicroAppClientExecuteLegacyAppsGameExtensions.cs
+++ src/DH.SKIT.FlurlHttpClient.ByteDance.MicroApp/Extensions/DouyinMicroAppClientExecuteLegacyAppsGameExtensions.cs
@@ -9,8 +9,7 @@ using Flurl.Http;
 
 namespace SKIT.FlurlHttpClient.ByteDance.MicroApp
 {
-    public static class DouyinMicroAppClientExecuteLegacyAppsGameExtensions
-    {
+    public static class DouyinMicroAppClientExecuteLegacyAppsGameExtensions {
         private static T PreprocessRequest<T>(DouyinMicroAppClient client, ref T request)
             where T : Models.AppsGameWalletRequestBase, new()
         {
@@ -31,12 +30,12 @@ namespace SKIT.FlurlHttpClient.ByteDance.MicroApp
             {
                 string json = client.JsonSerializer.Serialize(request);
                 IDictionary<string, string> paramMap = new SortedDictionary<string, string>(Newtonsoft.Json.JsonConvert.DeserializeObject<IDictionary<string, string>>(json)!, StringComparer.Ordinal);
-                foreach (var entry in paramMap)
+                foreach (string key in paramMap.Keys.ToArray())
                 {
-                    if (entry.Key == "mp_sig")
-                        paramMap.Remove(entry.Key);
-                    if (entry.Value is null)
-                        paramMap.Remove(entry.Key);
+                    if (key == "mp_sig")
+                        paramMap.Remove(key);
+                    else if (paramMap[key] is null)
+                        paramMap.Remove(key);
                 }
 
                 string message = string.Join("&", paramMap.Select(e => $"{e.Key}={e.Value}"))
