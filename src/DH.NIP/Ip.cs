0fe05757993349ea24b4c5caee8f4f4f9a41886e
猿人易 <qcjxberin@outlook.com>
2024-08-05 10:55:14 +0800

    [feat]v2.2 使用MMF访问数据库文件，避免内存缓存造成内存浪费 静态构造函数里不能等待，否则异步函数也无法执行 支持外部指定DbFile 数据流改为访问器读取，首次测试通过，最终目标是为了解决多线程并行访问的问题 遍历IP数据库 数据库新增GetIndex，支持获取指定索引处的信息 支持多线程并行查询，由于不再使用数据流，改用访问器 优化结构信息返回，地址和运营商分开

--- src/DH.NIP/Ip.cs
+++ src/DH.NIP/Ip.cs
@@ -6,157 +6,143 @@ using NewLife.Web;
 namespace NewLife.IP;
 
 /// <summary>IP搜索</summary>
-public static class Ip
+public class Ip
 {
-    private static readonly Object lockHelper = new();
-    private static Zip zip;
+    private IpDatabase _zip;
 
     /// <summary>数据文件</summary>
-    public static String DbFile { get; set; } = "";
+    public String DbFile { get; set; } = "";
+
+    /// <summary>数据库实例</summary>
+    public IpDatabase Db => _zip;
 
     static Ip()
     {
 #if NETCOREAPP
         Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 #endif
-        var set = Setting.Current;
-        var dir = set.DataPath;
-        var ip = dir.CombinePath("ip.gz").GetBasePath();
-        if (File.Exists(ip)) DbFile = ip;
-
-        // 如果本地没有IP数据库，则从网络下载
-        var fi = DbFile.IsNullOrWhiteSpace() ? null : DbFile.AsFile();
-        if (fi == null || !fi.Exists || fi.LastWriteTime < new DateTime(2024, 03, 09))
-        {
-            var task = Task.Factory.StartNew(() =>
-            {
-                var url = set.PluginServer;
-                if (fi == null || !fi.Exists)
-                    XTrace.WriteLine("没有找到IP数据库{0}，准备联网获取 {1}", ip, url);
-                else
-                    XTrace.WriteLine("IP数据库{0}已过期，准备联网更新 {1}", ip, url);
-
-                // 无法下载ip地址库时，不要抛出异常影响业务层
-                try
-                {
-                    var client = new WebClientX
-                    {
-                        Log = XTrace.Log
-                    };
-                    var file = client.DownloadLink(url, "ip.gz", Path.GetTempPath());
-
-                    if (File.Exists(file))
-                    {
-                        if (File.Exists(ip)) File.Delete(ip);
-                        ip.EnsureDirectory(true);
-                        File.Move(file, ip);
-
-                        DbFile = ip;
-                        zip = null;
-                        // 让它重新初始化
-                        _inited = null;
-                    }
-                }
-                catch (Exception ex)
-                {
-                    XTrace.WriteException(ex);
-                }
-            });
-            task.Wait(3_000);
-        }
     }
 
-    static Boolean? _inited;
-    static Boolean Init()
+    private Boolean? _inited;
+    /// <summary>初始化</summary>
+    /// <returns></returns>
+    public Boolean Init()
     {
         if (_inited != null) return _inited.Value;
         lock (typeof(Ip))
         {
             if (_inited != null) return _inited.Value;
-            _inited = false;
 
-            var z = new Zip();
-
-            if (!File.Exists(DbFile))
+            var ip = DbFile;
+            if (ip.IsNullOrEmpty())
             {
-                //throw new InvalidOperationException("无法找到IP数据库" + DbFile + "！");
-                XTrace.WriteLine("无法找到IP数据库{0}", DbFile);
+                var set = Setting.Current;
+                ip = set.DataPath.CombinePath("ip.gz").GetBasePath();
+                if (File.Exists(ip)) DbFile = ip;
+            }
+
+            // 如果本地没有IP数据库，则从网络下载
+            var fi = ip.IsNullOrWhiteSpace() ? null : ip.AsFile();
+            if (fi == null || !fi.Exists || fi.Length < 3 * 1024 * 1024 || fi.LastWriteTime < new DateTime(2024, 03, 09))
+            {
+                var task = Task.Run(() => Download(ip));
+                // 静态构造函数里不能等待，否则异步函数也无法执行
+                task.Wait(5_000);
+            }
+
+            var zip = new IpDatabase();
+            ip = DbFile;
+            if (!File.Exists(ip))
+            {
+                XTrace.WriteLine("无法找到IP数据库{0}", ip);
                 return false;
             }
-            XTrace.WriteLine("使用IP数据库{0}", DbFile);
-            using (var fs = File.OpenRead(DbFile))
+            XTrace.WriteLine("IP数据库：{0}", ip);
+
+            try
             {
-                try
-                {
-                    z.SetStream(fs);
-                }
-                catch (Exception ex)
-                {
-                    XTrace.WriteException(ex);
+                zip.SetFile(ip);
 
-                    return false;
-                }
+                _zip = zip;
             }
-            if (z.Stream != null) zip = z;
-        }
+            catch (Exception ex)
+            {
+                _inited = false;
+                XTrace.WriteException(ex);
 
-        //if (zip.Stream == null) throw new InvalidOperationException("无法打开IP数据库" + DbFile + "！");
+                return false;
+            }
+        }
 
         _inited = true;
         return true;
     }
 
+    private Boolean Download(String ip)
+    {
+        var set = Setting.Current;
+        var url = set.PluginServer;
+
+        var fi = ip.AsFile();
+        if (fi == null || !fi.Exists)
+            XTrace.WriteLine("没有找到IP数据库{0}，准备联网获取 {1}", ip, url);
+        else
+            XTrace.WriteLine("IP数据库{0}已过期，准备联网更新 {1}", ip, url);
+
+        // 无法下载ip地址库时，不要抛出异常影响业务层
+        try
+        {
+            var client = new WebClientX
+            {
+                Log = XTrace.Log
+            };
+            var file = client.DownloadLink(url, "ip.gz", Path.GetTempPath());
+
+            if (File.Exists(file))
+            {
+                if (File.Exists(ip)) File.Delete(ip);
+                ip.EnsureDirectory(true);
+                File.Move(file, ip);
+
+                DbFile = ip;
+
+                // 下载成功时，让它重新初始化
+                _inited = null;
+
+                return true;
+            }
+        }
+        catch (Exception ex)
+        {
+            XTrace.WriteException(ex);
+        }
+
+        return false;
+    }
+
     /// <summary>获取IP地址</summary>
     /// <param name="ip"></param>
     /// <returns></returns>
-    public static String GetAddress(String ip)
+    public String GetAddress(String ip)
     {
         if (String.IsNullOrEmpty(ip)) return "";
 
-        if (!Init() || zip == null) return "";
+        if (!Init() || _zip == null) return "";
 
-        var ip2 = IPToUInt32(ip.Trim());
-        lock (lockHelper)
-        {
-            return zip.GetAddress(ip2) + "";
-        }
+        var (addr, area) = _zip.GetAddress(ip.Trim().ToUInt32IP());
+        return addr + " " + area;
     }
 
     /// <summary>获取IP地址</summary>
     /// <param name="addr"></param>
     /// <returns></returns>
-    public static String GetAddress(IPAddress addr)
+    public String GetAddress(IPAddress ip)
     {
-        if (addr == null) return "";
+        if (ip == null) return "";
 
-        if (!Init() || zip == null) return "";
+        if (!Init() || _zip == null) return "";
 
-        var buf = addr.GetAddressBytes();
-        Array.Reverse(buf);
-        var ip2 = (UInt32)buf.ToInt();
-        lock (lockHelper)
-        {
-            return zip.GetAddress(ip2) + "";
-        }
-    }
-
-    static UInt32 IPToUInt32(String IpValue)
-    {
-        var ss = IpValue.Split('.');
-        //var buf = stackalloc Byte[4];
-        var val = 0u;
-        //var ptr = (Byte*)&val;
-        for (var i = 0; i < 4; i++)
-        {
-            if (i < ss.Length && UInt32.TryParse(ss[i], out var n))
-            {
-                //buf[3 - i] = (Byte)n;
-                // 感谢啊富弟（QQ125662872）指出错误，右边需要乘以8，这里为了避开乘法，采用位移实现
-                val |= n << ((3 - i) << 3);
-                //ptr[3 - i] = n;
-            }
-        }
-        //return BitConverter.ToUInt32(buf, 0);
-        return val;
+        var (addr, area) = _zip.GetAddress(ip.ToUInt32());
+        return addr + " " + area;
     }
 }
---
+++ src/DH.NIP/IpDatabase.cs
@@ -0,0 +1,212 @@
+﻿using System.IO.MemoryMappedFiles;
+using System.Text;
+using NewLife.Log;
+
+namespace NewLife.IP;
+
+/// <summary>IP数据库</summary>
+public class IpDatabase : IDisposable
+{
+    #region 属性
+    /// <summary>起始位置</summary>
+    public UInt32 Start { get; private set; }
+
+    /// <summary>结束位置</summary>
+    public UInt32 End { get; private set; }
+
+    /// <summary>记录数</summary>
+    public UInt32 Count { get; private set; }
+
+    MemoryMappedFile _mmf;
+    MemoryMappedViewAccessor _view;
+    String _tempFile;
+    #endregion
+
+    #region 构造
+    /// <summary>析构</summary>
+    ~IpDatabase() { OnDispose(false); }
+
+    /// <summary>销毁</summary>
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+
+        OnDispose(true);
+    }
+
+    void OnDispose(Boolean disposing)
+    {
+        if (disposing)
+        {
+            _mmf.TryDispose();
+
+            if (!_tempFile.IsNullOrEmpty() && File.Exists(_tempFile)) File.Delete(_tempFile);
+
+            GC.SuppressFinalize(this);
+        }
+    }
+    #endregion
+
+    #region 数据源
+    /// <summary>设置文件</summary>
+    /// <param name="file"></param>
+    /// <returns></returns>
+    public IpDatabase SetFile(String file)
+    {
+        // 仅支持Gzip压缩，可用7z软件先压缩为gz格式
+        if (file.EndsWithIgnoreCase(".gz"))
+        {
+            // 解压缩到临时文件
+            var file2 = Path.GetTempFileName();
+
+            using var fs2 = File.Create(file2);
+            using var fs = File.OpenRead(file);
+            fs.DecompressGZip(fs2);
+
+            // 重新打开，切换到文件流
+            file = file2;
+            _tempFile = file2;
+        }
+
+        // 启用MMF
+        _mmf = MemoryMappedFile.CreateFromFile(file, FileMode.Open, null, 0, MemoryMappedFileAccess.ReadWrite);
+
+        using var view = _mmf.CreateViewAccessor();
+        Start = view.ReadUInt32(0);
+        End = view.ReadUInt32(4);
+        Count = (End - Start) / 7u + 1u;
+
+        XTrace.WriteLine("IP记录数：{0:n0}", Count);
+
+        return this;
+    }
+    #endregion
+
+    #region 方法
+    /// <summary>获取IP的物理地址</summary>
+    /// <param name="ip"></param>
+    /// <returns></returns>
+    public (String addr, String area) GetAddress(UInt32 ip)
+    {
+        var idxSet = 0u;
+        var idxEnd = Count - 1u;
+        // 频繁销毁视图会导致性能下降
+        //using var view = _mmf.CreateViewAccessor();
+        var view = _view ??= _mmf.CreateViewAccessor();
+
+        // 二分法搜索，找到IP所在区间
+        IndexInfo set;
+        while (true)
+        {
+            set = ReadIndexInfo(view, idxSet);
+            if (ip >= set.Start && ip <= set.End) break;
+
+            var end = ReadIndexInfo(view, idxEnd);
+            if (ip >= end.Start && ip <= end.End) return ReadAddressInfo(view, end.Offset);
+
+            var mid = ReadIndexInfo(view, (idxEnd + idxSet) / 2u);
+            if (ip >= mid.Start && ip <= mid.End) return ReadAddressInfo(view, mid.Offset);
+
+            if (ip < mid.Start)
+                idxEnd = (idxEnd + idxSet) / 2u;
+            else
+                idxSet = (idxEnd + idxSet) / 2u;
+        }
+        return ReadAddressInfo(view, set.Offset);
+    }
+
+    /// <summary>获取指定索引处的信息</summary>
+    /// <param name="idx"></param>
+    /// <returns></returns>
+    public (IndexInfo, String addr, String area) GetIndex(UInt32 idx)
+    {
+        var view = _view ??= _mmf.CreateViewAccessor();
+
+        var info = ReadIndexInfo(view, idx);
+        var (addr, area) = ReadAddressInfo(view, info.Offset);
+        return (info, addr, area);
+    }
+
+    (String addr, String area) ReadAddressInfo(UnmanagedMemoryAccessor view, UInt32 offset)
+    {
+        String addr;
+        String area;
+
+        var p = offset + 4;
+        var tag = view.ReadByte(p);
+        if (tag == 1)
+        {
+            p = view.ReadUInt32(p + 1) & 0x00FF_FFFF;
+            tag = view.ReadByte(p);
+            if (tag == 2)
+            {
+                offset = view.ReadUInt32(p + 1) & 0x00FF_FFFF;
+                area = ReadArea(view, p + 4);
+                addr = ReadString(view, ref offset);
+            }
+            else
+            {
+                addr = ReadString(view, ref p);
+                area = ReadArea(view, p);
+            }
+        }
+        else
+        {
+            if (tag == 2)
+            {
+                offset = view.ReadUInt32(p + 1) & 0x00FF_FFFF;
+                area = ReadArea(view, p + 4);
+                addr = ReadString(view, ref offset);
+            }
+            else
+            {
+                addr = ReadString(view, ref p);
+                area = ReadArea(view, p);
+            }
+        }
+        return (addr, area);
+    }
+
+    String ReadArea(UnmanagedMemoryAccessor view, UInt32 p)
+    {
+        var tag = view.ReadByte(p);
+        if (tag == 1 || tag == 2)
+            p = view.ReadUInt32(p + 1) & 0x00FF_FFFF;
+
+        return ReadString(view, ref p);
+    }
+
+    private static Encoding _encoding;
+    String ReadString(UnmanagedMemoryAccessor view, ref UInt32 p)
+    {
+        var buf = new Byte[256];
+        var count = view.ReadArray(p, buf, 0, buf.Length);
+
+        var k = 0u;
+        while (k < count && buf[k] != 0) k++;
+        if (k == 0) return String.Empty;
+
+        p += k;
+
+        _encoding ??= Encoding.GetEncoding("GB2312");
+
+        var str = _encoding.GetString(buf, 0, (Int32)k).Trim().Trim('\0').Trim();
+        if (str == "CZ88.NET") return String.Empty;
+
+        return str;
+    }
+
+    IndexInfo ReadIndexInfo(UnmanagedMemoryAccessor view, UInt32 index)
+    {
+        var p = Start + 7u * index;
+        var inf = new IndexInfo
+        {
+            Start = view.ReadUInt32(p),
+            Offset = view.ReadUInt32(p + 4) & 0x00FF_FFFF
+        };
+        inf.End = view.ReadUInt32(inf.Offset);
+
+        return inf;
+    }
+    #endregion
+}
