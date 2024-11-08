069d4f9ed71c13be20aeffddebd12a5b861164e9
猿人易 <qcjxberin@outlook.com>
2024-11-08 18:04:20 +0800

    删除 `ToSafeString` 方法
    删除了 `ToSafeString` 方法，该方法用于将对象转换为字符串，如果对象为 null 则返回空字符串。

--- src/DH.Common/Extensions/CoreExtension.cs
+++ src/DH.Common/Extensions/CoreExtension.cs
@@ -2557,16 +2557,6 @@ public static class CoreExtension
         return typeof(T).IsValueType ? source.Equals(default(T)) : source == null;
     }
 
-    /// <summary>
-    ///     An object extension method that converts the @this to string or return an empty string if the value is null.
-    /// </summary>
-    /// <param name="this">The @this to act on.</param>
-    /// <returns>@this as a string or empty if the value is null.</returns>
-    public static string ToSafeString(this object @this)
-    {
-        return @this == null ? string.Empty : @this.ToString();
-    }
-
     #endregion object
 
     #region object[]
