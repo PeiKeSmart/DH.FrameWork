050a90fc7e469728bb2d0f1ade5afded95d81961
qcjxberin <2505111990@qq.com>
2025-06-23 00:39:54 +0800

    清理Wechat

--- src/DH.SKIT.FlurlHttpClient.Wechat.Work/ExtendedSDK/SpecialApi/Models/SpecialExecuteAndReportJobResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace SKIT.FlurlHttpClient.Wechat.Work.ExtendedSDK.SpecialApi.Models
-{
-    /// <summary>
-    /// <para>表示 [POST] /spec/exec_and_report_job 接口的响应。</para>
-    /// </summary>
-    public class SpecialExecuteAndReportJobResponse : WechatWorkSpecialApiResponse
-    {
-    }
-}
