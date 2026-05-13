6a2321203140d43b1a8bdd5905f4a896f61c93d5
猿人易 <qcjxberin@outlook.com>
2026-05-13 11:43:02 +0800

    清理

--- demo/ScuiDemo/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ScuiDemo.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
