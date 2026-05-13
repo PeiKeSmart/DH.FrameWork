0c715006442fb5e55b46560380f556122444a9df
猿人易 <qcjxberin@outlook.com>
2026-05-13 14:42:09 +0800

    清理

--- src/DH.Magicodes.IE.EPPlus/FormulaParsing/Utilities/IdProvider.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace OfficeOpenXml.FormulaParsing.Utilities
-{
-    public abstract class IdProvider
-    {
-        public abstract object NewId();
-    }
-}
--- src/DH.Magicodes.IE.EPPlus/Utils/IValidationResult.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace OfficeOpenXml.Utils
-{
-    public interface IValidationResult
-    {
-        void IsTrue();
-        void IsFalse();
-    }
-}
